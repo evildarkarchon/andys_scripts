3023957ed48dd9fa2229e71d751f1c481ae2cba4
Andrew Nelson <andrew@andrewnelson.org>
2016-02-17 03:26:01 -0800

    Use pathlib to ensure that filename is properly formatted.

--- andy/hashfile.py
+++ andy/hashfile.py
@@ -1,9 +1,11 @@
 import hashlib
+import pathlib
 
 def hashfile(filename):
     BLOCKSIZE = 65536
     hasher = hashlib.sha512()
-    with open(filename, 'rb') as afile:
+    filepath=pathlib.Path(filename)
+    with open(str(filepath), 'rb') as afile:
         buf = afile.read(BLOCKSIZE)
         while len(buf) > 0:
             hasher.update(buf)
