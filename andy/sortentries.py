4f9f86a5389e00292e45142eacb00347db0258b5
Andrew Nelson <andrew@andrewnelson.org>
2016-02-16 16:08:31 -0800

    Make sortentries and built-in sorting sections more consistent.

--- andy/sortentries.py
+++ andy/sortentries.py
@@ -3,23 +3,31 @@ try:
     natsortpresent=True
 except ImportError:
     natsortpresent=False
+    print("natsort not found.")
     pass
 
 from andy.colors import Color
+from andy.prettylist import prettylist
+from andy.flatten import flatten
 
 colors=Color()
 
 def sortentries(text):
     if natsortpresent:
-        return humansorted(text)
+        if type(text) in (str,):
+            text=prettylist(text, sep=' ').split()
+            return humansorted(text.split())
+        if type(text) in (tuple, list):
+            text=prettylist(text, sep=' ').split()
+            return humansorted(text)
+        else:
+            print("{} Argument must be a string, tuple, or list.".format(colors.mood("sad")))
     else:
         if type(text) in (str,):
             return sorted(text.split(), key=str.lower)
-        elif type(text) in (tuple,):
-            list(text)
-            return sorted(text, key=str.lower)
-        elif type(text) in (list,):
+        elif type(text) in (tuple, list):
+            text=prettylist(text, sep=' ').split()
             return sorted(text, key=str.lower)
         else:
-            print("{} Argument must be a string, tuple, or list".format(colors.mood("sad")))
+            print("{} Argument must be a string, tuple, or list.".format(colors.mood("sad")))
             raise ValueError
