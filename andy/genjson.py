5bc433d46f3824e0d7373382ccb355d54be3fee1
Andrew Nelson <andrew@andrewnelson.org>
2016-02-18 04:08:31 -0800

    fix some wrong variable calls.

--- andy/genjson.py
+++ andy/genjson.py
@@ -7,16 +7,17 @@ colors=Color()
 
 def genjson(filename, dictionary):
     jsonpath=pathlib.Path(filename)
+#    print(dictionary)
 
     if not isinstance(dictionary, dict):
         print("{} Second argument must be a dictionary.".format(colors.mood("sad")))
         raise TypeError
 
     if jsonpath.exists():
-        print("{} Backing up {} to {}".format(colors.mood("happy"), filename, filename.replace(".json", ".json.bak")))
-        with open(filename) as orig, open(filename.replace(".json", ".json.bak"), "w") as backup:
+        print("{} Backing up {} to {}".format(colors.mood("happy"), str(jsonpath), str(jsonpath).replace(".json", ".json.bak")))
+        with open(str(jsonpath)) as orig, open(str(jsonpath).replace(".json", ".json.bak"), "w") as backup:
             backup.write(orig.read())
 
-    with open(jsonfile) as dest:
-        print("{} Writing values to JSON file: {}".format(colors.mood("happy"), jsonfile))
-        json.dumps(dictionary, sort_keys=True, indent="\t")
+    with open(str(jsonpath), "w") as dest:
+        print("{} Writing values to JSON file: {}".format(colors.mood("happy"), str(jsonpath)))
+        dest.write(json.dumps(dictionary, sort_keys=True, indent="\t"))
