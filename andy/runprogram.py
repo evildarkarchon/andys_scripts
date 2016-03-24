ecdb58267442488ef99d19a409b405f5ef2f620a
Andrew Nelson <andrew@andrewnelson.org>
2016-03-24 03:53:40 -0700

    use a deque for the command variable for easier insertion of the sudo command (the list for the sudo portion has to be backwords because it prepends in the order specified)

--- andy/runprogram.py
+++ andy/runprogram.py
@@ -1,28 +1,43 @@
 import subprocess
+import pwd
+import shlex
+
+from collections import deque
 
 from andy.python_version import is_python_version
 from andy.colors import Color
 
 colors=Color()
 
-def runprogram(program, verify=True, use_sudo=False, user="root", stdinput=None, stdoutput=None, stderror=None, environment=None):
+def runprogram(program, verify=True, use_sudo=False, user="root", stdinput=None, stdoutput=None, stderror=None, environment=None, workdir=None):
 
-    if type(program) in (list, tuple):
-        command=list(program)
+    if type(program) in (tuple,):
+        command=deque(program)
     elif type(program) in (str,):
-        command=program.split()
+        command=deque(shlex.split(program))
+    elif type(program) in (list,):
+        command=deque(program)
     else:
         print("{} program must be in the form of a string, tuple, or list")
 
+    if use_sudo and type(user) not in (str, int):
+        print("{} User must be a string or integer.".format(colors.mood("sad")))
+        raise TypeError
+
+    if use_sudo and type(user) in (int,):
+        uid=pwd.getpwuid(user)
+        user=uid.pw_name
+
     if use_sudo:
-        command.insert(0, "sudo")
-        command.insert(1, "-u")
-        command.insert(2, user)
+        #command.insert(0, "sudo")
+        #command.insert(1, "-u")
+        #command.insert(2, user)
+        command.extendleft([user, "-u", "sudo"]) #has to be backwords because each entry is prepended to the beginning of the list in the state its at when it gets to that point in the list.
 
     if is_python_version((3,5,0)):
-        subprocess.run(command, input=stdinput, stdout=stdoutput, stderr=stderror, env=environment, check=verify)
+        subprocess.run(command, input=stdinput, stdout=stdoutput, stderr=stderror, env=environment, check=verify, cwd=workdir)
     else:
         if verify:
-            subprocess.check_call(command, stdin=stdinput, stdout=stdoutput, stderr=stderror, env=environment)
+            subprocess.check_call(command, stdin=stdinput, stdout=stdoutput, stderr=stderror, env=environment, cwd=workdir)
         else:
-            subprocess.call(command, stdin=stdinput, stdout=stdoutput, stderr=stderror, env=environment)
+            subprocess.call(command, stdin=stdinput, stdout=stdoutput, stderr=stderror, env=environment, cwd=workdir)
