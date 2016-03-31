817da328f8b067235fd72a5279925d58cc6ddede
Andrew Nelson <andrew@andrewnelson.org>
2016-03-30 20:48:06 -0700

    Switch hashfile from sha512 to sha256 (sha512 is overkill for my purposes.)

--- andy/hashfile.py
+++ andy/hashfile.py
@@ -3,11 +3,12 @@ import pathlib
 
 def hashfile(filename):
     BLOCKSIZE = 65536
-    hasher = hashlib.sha512()
-    filepath=pathlib.Path(filename)
+    hasher = hashlib.sha256()
+    filepath=pathlib.Path(filename).resolve()
     with open(str(filepath), 'rb') as afile:
         buf = afile.read(BLOCKSIZE)
         while len(buf) > 0:
             hasher.update(buf)
             buf = afile.read(BLOCKSIZE)
+#        hasher.update(afile.read())
     return hasher.hexdigest()
