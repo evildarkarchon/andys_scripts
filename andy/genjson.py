35c4a15f7c6d80b4c63f8d29fdd3e2d3eaa0d1e3
Andrew Nelson <andrew@andrewnelson.org>
2016-04-09 03:33:38 -0700

    Add code in genjson to just print the resulting json.

--- andy/genjson.py
+++ andy/genjson.py
@@ -6,19 +6,21 @@ from andy.colors import Color
 
 colors=Color()
 
-def genjson(filename, dictionary):
+def genjson(filename, dictionary, printdata=False):
     jsonpath=pathlib.Path(filename).resolve()
 #    print(dictionary)
 
     if not isinstance(dictionary, (dict, collections.ChainMap, collections.OrderedDict, collections.defaultdict)):
         print("{} Second argument must be a dictionary.".format(colors.mood("sad")))
         raise TypeError
+    if not printdata:
+        if jsonpath.exists():
+            print("{} Backing up {} to {}".format(colors.mood("happy"), str(jsonpath), str(jsonpath).replace(".json", ".json.bak")))
+            with open(str(jsonpath)) as orig, open(str(jsonpath).replace(".json", ".json.bak"), "w") as backup:
+                backup.write(orig.read())
 
-    if jsonpath.exists():
-        print("{} Backing up {} to {}".format(colors.mood("happy"), str(jsonpath), str(jsonpath).replace(".json", ".json.bak")))
-        with open(str(jsonpath)) as orig, open(str(jsonpath).replace(".json", ".json.bak"), "w") as backup:
-            backup.write(orig.read())
-
-    with open(str(jsonpath), "w") as dest:
-        print("{} Writing values to JSON file: {}".format(colors.mood("happy"), str(jsonpath)))
-        dest.write(json.dumps(dictionary, sort_keys=True, indent="\t"))
+        with open(str(jsonpath), "w") as dest:
+            print("{} Writing values to JSON file: {}".format(colors.mood("happy"), str(jsonpath)))
+            dest.write(json.dumps(dictionary, sort_keys=True, indent="\t"))
+    else:
+        print(json.dumps(dictionary, sort_keys=True, indent="\t"))
