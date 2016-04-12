555416001823069d001bc729977ba192dc1c1d63
Andrew Nelson <andrew@andrewnelson.org>
2016-04-11 18:03:53 -0700

    Add stdin to returninfo

--- andy/returninfo.py
+++ andy/returninfo.py
@@ -5,11 +5,11 @@ from andy.colors import Color
 
 colors=Color()
 
-def returninfo(source, string=True):
+def returninfo(source, string=True, stdinput=None):
     if not isinstance(source, (list, tuple, collections.deque)):
         print("{} Source must be a list, tuple, or deque")
         raise TypeError
-    data=subprocess.Popen(source, stdout=subprocess.PIPE, stderr=subprocess.DEVNULL)
+    data=subprocess.Popen(source, stdin=stdinput, stdout=subprocess.PIPE, stderr=subprocess.DEVNULL)
     if string:
         return data.communicate()[0].decode("utf-8")
     else:
