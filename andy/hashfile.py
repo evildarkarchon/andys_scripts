5a3d3868cf40de1751395e27f4b3c7d6721a8916
Andrew Nelson <andrew@andrewnelson.org>
2016-04-14 16:06:54 -0700

    Uncapitalize blocksize variable name.

--- andy/hashfile.py
+++ andy/hashfile.py
@@ -2,13 +2,13 @@ import hashlib
 import pathlib
 
 def hashfile(filename):
-    BLOCKSIZE = 65536
+    blocksize = 65536
     hasher = hashlib.sha256()
     filepath=pathlib.Path(filename).resolve()
     with open(str(filepath), 'rb') as afile:
-        buf = afile.read(BLOCKSIZE)
+        buf = afile.read(blocksize)
         while len(buf) > 0:
             hasher.update(buf)
-            buf = afile.read(BLOCKSIZE)
+            buf = afile.read(blocksize)
 #        hasher.update(afile.read())
     return hasher.hexdigest()
