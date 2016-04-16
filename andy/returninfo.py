14c16d4b5d577ff819219962026e10b3f9e0d1be
Andrew Nelson <andrew@andrewnelson.org>
2016-04-15 22:27:52 -0700

    Don't do our own type checking, instead, if a string is passed, use shlex to make it into a proper list.

--- andy/returninfo.py
+++ andy/returninfo.py
@@ -1,14 +1,14 @@
 import collections
 import subprocess
+import shlex
 
 from andy.colors import Color
 
 colors=Color()
 
 def returninfo(source, string=True, stdinput=None):
-    if not isinstance(source, (list, tuple, collections.deque)):
-        print("{} Source must be a list, tuple, or deque")
-        raise TypeError
+    if isinstance(source, str):
+        source=shlex.split(source)
     data=subprocess.Popen(source, stdin=stdinput, stdout=subprocess.PIPE, stderr=subprocess.DEVNULL)
     if string:
         return data.communicate()[0].decode("utf-8")
