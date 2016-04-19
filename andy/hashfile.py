73e09ced5a1815a267496605c7e96d0cc556145f
Andrew Nelson <andrew@andrewnelson.org>
2016-04-19 02:29:56 -0700

    Use python buffering instead of a while loop that iterates over the data.

--- andy/hashfile.py
+++ andy/hashfile.py
@@ -1,15 +1,9 @@
 import hashlib
 import pathlib
 
-
 def hashfile(filename):
-    blocksize = 65536
-    hasher = hashlib.sha256()
+    hasher=hashlib.sha256()
     filepath=pathlib.Path(filename).resolve()
-    with open(str(filepath), 'rb') as afile:
-        buf = afile.read(blocksize)
-        while len(buf) > 0:
-            hasher.update(buf)
-            buf = afile.read(blocksize)
-#        hasher.update(afile.read())
+    with open(str(filepath), "rb", 200000000) as afile:
+        hasher.update(afile.read())
     return hasher.hexdigest()
