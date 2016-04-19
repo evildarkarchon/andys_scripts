422e1664587a8a4aad01587f909b21ebaa2b2169
Andrew Nelson <andrew@andrewnelson.org>
2016-04-19 02:33:09 -0700

    Remove the whole natsortpresent conditional stuff, instead, use a try/except clause.

--- andy/sortentries.py
+++ andy/sortentries.py
@@ -3,30 +3,27 @@ import collections
 from andy.colors import Color
 from andy.flatten import flatten
 from andy.prettylist import prettylist
-
 try:
     from natsort import humansorted
-    natsortpresent=True
 except ImportError:
-    natsortpresent=False
-    print("natsort not found.")
     pass
 
 
 colors=Color()
 
 def sortentries(text):
-    if natsortpresent:
+    try:
         if isinstance(text, str):
             return humansorted(text.split())
         elif isinstance(text, (list, collections.deque)):
             return humansorted(text)
         else:
             return list(humansorted(text))
-    else:
+    except NameError:
         if isinstance(text, str):
             return sorted(text.split(), key=str.lower)
         elif isinstance(text, (list, collections.deque)):
             return sorted(text, key=str.lower)
         else:
             return list(sorted(text, key=str.lower))
+        pass
