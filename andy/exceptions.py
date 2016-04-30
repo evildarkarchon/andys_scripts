4cb39241f61f43e52cb6080e24bcdf4f164ab1c4
Andrew Nelson <andrew@andrewnelson.org>
2016-04-29 19:34:01 -0700

    add message variable to VarLengthError

--- andy/exceptions.py
+++ andy/exceptions.py
@@ -3,7 +3,7 @@ from andy.colors import Color
 colors=Color()
 
 class VarLengthError(Exception): # This code is a work in progress (may not even keep this name).
-    def __init__(self, var, length):
+    def __init__(self, var, length, message=None):
         self.errorstr="\n{} {} must have {} entries.".format(colors.mood("sad"), var, length)
     def __str__(self):
         return str(self.errorstr)
