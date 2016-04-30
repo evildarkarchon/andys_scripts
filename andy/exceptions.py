b6de85ada4fb8cd5df4467c96933b4d9d680e738
Andrew Nelson <andrew@andrewnelson.org>
2016-04-29 18:35:20 -0700

    Change the NoneError exception to VarLengthError, its still a work in progress.

--- andy/exceptions.py
+++ andy/exceptions.py
@@ -2,8 +2,8 @@ from andy.colors import Color
 
 colors=Color()
 
-class NoneError(Exception): # This code is a work in progress (may not even keep this name).
-    def __init__(self):
-        self.errorstr="\n{} Value is set to None when it shouldn't be.".format(colors.mood("sad"))
+class VarLengthError(Exception): # This code is a work in progress (may not even keep this name).
+    def __init__(self, var, length):
+        self.errorstr="\n{} {} must have {} entries.".format(colors.mood("sad"), var, length)
     def __str__(self):
         return str(self.errorstr)
