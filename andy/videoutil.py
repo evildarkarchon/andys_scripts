b496e78fcb0f40af0ca7d9356590e97a59063cdf
Andrew Nelson <andrew@andrewnelson.org>
2016-05-09 02:26:57 -0700

    Partial pep8 changes for videoutil.py

--- andy/videoutil.py
+++ andy/videoutil.py
@@ -8,12 +8,14 @@ from andy.util import Color, Util, Program
 
 locale.setlocale(locale.LC_ALL, "en_US.utf-8")
 
+
 class VideoUtil:
+
     def __init__(self):
-        self.colors=Color()
-        self.util=Util()
-        self.program=Program()
-        self.mkvpropedit=shutil.which("mkvpropedit", mode=os.X_OK)
-        self.ffmpeg=shutil.which("ffmpeg", mode=os.X_OK)
-        self.mkvmerge=shutil.which("mkvmerge", mode=os.X_OK)
-        self.ffprobe=shutil.which("ffprobe", mode=os.X_OK)
+        self.colors = Color()
+        self.util = Util()
+        self.program = Program()
+        self.mkvpropedit = shutil.which("mkvpropedit", mode=os.X_OK)
+        self.ffmpeg = shutil.which("ffmpeg", mode=os.X_OK)
+        self.mkvmerge = shutil.which("mkvmerge", mode=os.X_OK)
+        self.ffprobe = shutil.which("ffprobe", mode=os.X_OK)
