a6a15b474f36d08bbfe28e314853606c29dacf81
Andrew Nelson <andrew@andrewnelson.org>
2016-05-09 02:24:47 -0700

    Migrate to new module, partial pep8 changes.

--- andy/git.py
+++ andy/git.py
@@ -1,17 +1,18 @@
 import pathlib
 from collections import deque
 
-from andy.colors import Color
-from andy.privileged import is_privileged
-from andy.runprogram import runprogram
+from andy.util import Color, Util, Program
 
 
 class Git:
+
     def __init__(self, directory, use_sudo=None, sudo_user=None, url=None):
-        self.colors=Color()
-        self.path=pathlib.Path(directory).resolve()
-        self.directory=str(self.path)
-        self.url=url
+        self.colors = Color()
+        self.util = Util()
+        self.program = Program()
+        self.path = pathlib.Path(directory).resolve()
+        self.directory = str(self.path)
+        self.url = url
 
         def sudocheck():
             if use_sudo not in (True, False, None):
@@ -21,7 +22,7 @@ class Git:
             if use_sudo is None:
                 print("{} use_sudo variable is unset, reverting to manual detection".format(self.colors.mood("neutral")))
                 if sudo_user:
-                    return is_privileged(privuser=sudo_user), sudo_user
+                    return self.util.is_privileged(privuser=sudo_user), sudo_user
                 else:
                     return False, None
             elif use_sudo and sudo_user:
@@ -29,28 +30,28 @@ class Git:
             elif use_sudo and not sudo_user:
                 return use_sudo, "root"
 
-        sudo=sudocheck()
-        self.use_sudo=sudo[0]
-        self.sudo_user=sudo[1]
+        sudo = sudocheck()
+        self.use_sudo = sudo[0]
+        self.sudo_user = sudo[1]
 
     def clean_lock(self):
         if self.path.joinpath(".git", "index.lock").exists():
             if self.use_sudo:
-                runprogram(["rm", str(self.path.joinpath(".git", "index.lock"))], use_sudo=self.use_sudo, user=self.sudo_user)
+                self.program.runprogram(["rm", str(self.path.joinpath(".git", "index.lock"))], use_sudo=self.use_sudo, user=self.sudo_user)
             else:
                 self.path.joinpath(".git", "index.lock").unlink()
 
     def clone(self):
         if not self.url:
-            print("{} url not defined.".format(colors.mood("sad")))
+            print("{} url not defined.".format(self.colors.mood("sad")))
             raise ValueError
-        runprogram(["git", "clone", self.url, self.directory], use_sudo=self.use_sudo, user=self.sudo_user)
+        self.program.runprogram(["git", "clone", self.url, self.directory], use_sudo=self.use_sudo, user=self.sudo_user)
 
     def gc(self, aggressive=False):
-        gccmd=deque(["git", "gc"])
+        gccmd = deque(["git", "gc"])
         if aggressive:
             gccmd.append("--aggressive")
-        runprogram(gccmd, workdir=self.directory, use_sudo=self.use_sudo, user=self.sudo_user)
+        self.program.runprogram(gccmd, workdir=self.directory, use_sudo=self.use_sudo, user=self.sudo_user)
 
     def pull(self):
-        runprogram(["git", "pull"], workdir=self.directory, use_sudo=self.use_sudo, user=self.sudo_user)
+        self.program.runprogram(["git", "pull"], workdir=self.directory, use_sudo=self.use_sudo, user=self.sudo_user)
