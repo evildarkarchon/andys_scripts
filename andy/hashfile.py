d1418b6cd39d2346360493fe6a2bb84c830e11b7
Andrew Nelson <andrew@andrewnelson.org>
2016-05-09 02:23:06 -0700

    Remove deprecated modules.

--- andy/hashfile.py
+++
@@ -1,9 +0,0 @@
-import hashlib
-import pathlib
-
-def hashfile(filename):
-    hasher=hashlib.sha256()
-    filepath=pathlib.Path(filename).resolve()
-    with open(str(filepath), "rb", 200000000) as afile:
-        hasher.update(afile.read())
-    return hasher.hexdigest()
--- andy/prettylist.py
+++
@@ -1,5 +0,0 @@
-def prettylist(text, quotes=False, sep=", "):
-    if quotes:
-        return sep.join(repr(e) for e in text)
-    else:
-        return sep.join(str(e) for e in text)
