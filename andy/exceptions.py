ea228cd15ebf22db843bec2d8ebec9e3a05705cc
Andrew Nelson <andrew@andrewnelson.org>
2016-05-09 02:23:50 -0700

    Partial pep8 changes to exceptions.py

--- andy/exceptions.py
+++ andy/exceptions.py
@@ -1,9 +1,12 @@
 from andy.colors import Color
 
-colors=Color()
+colors = Color()
 
-class VarLengthError(Exception): # This code is a work in progress (may not even keep this name).
+
+class VarLengthError(Exception):  # This code is a work in progress (may not even keep this name).
+
     def __init__(self, var, length, message=None):
-        self.errorstr="\n{} {} must have {} entries.".format(colors.mood("sad"), var, length)
+        self.errorstr = "\n{} {} must have {} entries.".format(colors.mood("sad"), var, length)
+
     def __str__(self):
         return str(self.errorstr)
