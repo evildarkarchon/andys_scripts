0a142e250d033df7e6842b61ce87ddd06b8cf87d
Andrew Nelson <andrew@andrewnelson.org>
2016-05-09 01:36:24 -0700

    Fix imports, instantiate classes in proper place, partial pep8 fixes (mostly whitespace and newline changes)

--- andy/abs.py
+++ andy/abs.py
@@ -3,47 +3,47 @@ import pathlib
 import shutil
 import sqlite3
 import subprocess
-from collections import deque
-import itertools
 
-from andy.colors import Color
-from andy.flatten import flatten
-from andy.runprogram import runprogram
+from andy.util import Color, Program, Util
 from andy.videoinfo import VideoInfo
 from andy.videoutil import VideoUtil
 
-colors=Color()
-
 locale.setlocale(locale.LC_ALL, "en_US.utf-8")
 
+
 class ABS(VideoInfo, VideoUtil):
+
     def __init__(self, database=str(pathlib.Path.cwd().joinpath("videoinfo.sqlite")), debug=None, backup=None, output=None, converttest=False):
         if pathlib.Path(database).exists():
             VideoInfo.__init__(self, database)
-            self.vi=VideoInfo(database)
+            self.vi = VideoInfo(database)
         VideoUtil.__init__(self)
-        self.debug=debug
+        self.debug = debug
+
+        self.colors = Color()
+        self.util = Util()
+        self.program = Program()
 
         if backup:
-            self.backuppath=pathlib.Path(backup).resolve()
-            self.backup=str(self.backuppath)
+            self.backuppath = pathlib.Path(backup).resolve()
+            self.backup = str(self.backuppath)
         else:
-            self.backuppath=None
-            self.backup=None
+            self.backuppath = None
+            self.backup = None
 
         if output:
-            self.outputpath=pathlib.Path(output).resolve()
+            self.outputpath = pathlib.Path(output).resolve()
         else:
-            self.outputpath=pathlib.Path.cwd()
+            self.outputpath = pathlib.Path.cwd()
 
-        self.output=str(self.outputpath)
+        self.output = str(self.outputpath)
 
-        self.converttest=converttest
+        self.converttest = converttest
 
-        self.auto=False
-        self.fr=False
+        self.auto = False
+        self.fr = False
 
-        self.nocodec=(None, "none", "copy")
+        self.nocodec = (None, "none", "copy")
 
         if not self.ffmpeg:
             print("{} ffmpeg not found, exiting.")
@@ -51,9 +51,9 @@ class ABS(VideoInfo, VideoUtil):
 
     def convert(self, filename, videocodec=None, videobitrate=None, audiocodec=None, audiobitrate=None, videocodecopts=None, audiocodecopts=None, audiofilteropts=None, container=None, framerate=None, passes=2):
 
-        filepath=pathlib.Path(filename).resolve()
-        outpath=self.outputpath.joinpath(filepath.with_suffix(container).name)
-        output=str(outpath)
+        filepath = pathlib.Path(filename).resolve()
+        outpath = self.outputpath.joinpath(filepath.with_suffix(container).name)
+        output = str(outpath)
 
         def frameratefilter():
             if framerate:
@@ -61,9 +61,9 @@ class ABS(VideoInfo, VideoUtil):
             elif self.vi and not framerate and videocodec not in self.nocodec:
                 if self.fr is False:
                     print("{} Frame Rate not specified, attempting to read from the database.".format(self.colors.mood("neutral")))
-                    self.frcount=True
+                    self.frcount = True
                 try:
-                    fr=self.vi.queryvideoinfosr("select frame_rate from videoinfo where filename=?", filepath.name)
+                    fr = self.vi.queryvideoinfosr("select frame_rate from videoinfo where filename=?", filepath.name)
                     return ["-filter:v", "fps={}".format(fr[0])]
                 except (sqlite3.Error, IndexError):
                     print("{} Frame Rate for {} not found in database, will rely on ffmpeg auto-detection.".format(self.colors.mood("neutral"), filename))
@@ -74,7 +74,7 @@ class ABS(VideoInfo, VideoUtil):
                 return None
 
         if videocodec in self.nocodec or not videocodec:
-            passes=1
+            passes = 1
 
         def commandlist(passno=None, passmax=passes):
             if passno is None and passmax is 2:
@@ -85,7 +85,7 @@ class ABS(VideoInfo, VideoUtil):
                 print("{} The maximum pass variable can only be 1 or 2.".format(self.colors.mood("sad")))
                 raise ValueError
 
-            if isinstance(passno, int) and (passno >=2 and passmax is 1):
+            if isinstance(passno, int) and (passno >= 2 and passmax is 1):
                 print("{} is >=2 and the maximum number of passes is set to 1.".format(self.colors.mood("sad")))
                 raise ValueError
 
@@ -93,10 +93,10 @@ class ABS(VideoInfo, VideoUtil):
                 if self.database:
                     if self.auto is False:
                         print("{} Bit-rates not specified, attempting to guess from database entries.".format(self.colors.mood("neutral")))
-                        self.auto=True
+                        self.auto = True
 
-                    streams=self.vi.queryvideoinfosr("select streams from videoinfo where filename=?", filepath.name)[0]
-                    bitrates=self.vi.queryvideoinfosr("select bitrate_0_raw, bitrate_1_raw from videoinfo where filename=?", filepath.name)
+                    streams = self.vi.queryvideoinfosr("select streams from videoinfo where filename=?", filepath.name)[0]
+                    bitrates = self.vi.queryvideoinfosr("select bitrate_0_raw, bitrate_1_raw from videoinfo where filename=?", filepath.name)
                     if streams >= 2:
                         return [bitrates[0], bitrates[1]]
                     elif streams is 1:
@@ -104,67 +104,66 @@ class ABS(VideoInfo, VideoUtil):
                 else:
                     return None
             if ('videobitrate' not in vars() and 'audiobitrate' not in vars()) or (not videobitrate and not audiobitrate):
-                bitrates=auto_bitrates()
+                bitrates = auto_bitrates()
                 if self.debug:
                     print(bitrates)
 
             if ('videobitrate' not in vars() or not videobitrate) and videocodec not in self.nocodec and len(bitrates) is 2:
-                videobitrate=str(max(bitrates))
+                videobitrate = str(max(bitrates))
                 if self.debug:
                     print(videobitrate)
             elif 'videobitrate' not in vars() and videocodec not in self.nocodec and ('audiocodec' not in vars() or not audiocodec) and len(bitrates) is 1:
-                videobitrate=str(bitrates)
+                videobitrate = str(bitrates)
                 if self.debug:
                     print(videobitrate)
 
             if 'audiobitrate' not in vars() and audiocodec not in self.nocodec and len(bitrates) is 2:
-                audiobitrate=str(min(bitrates))
+                audiobitrate = str(min(bitrates))
                 if self.debug:
                     print(audiobitrate)
             elif 'audiobitrate' not in vars() and audiocodec not in self.nocodec and len(bitrates) is 1:
-                audiobitrate=str(bitrates)
+                audiobitrate = str(bitrates)
                 if self.debug:
                     print(audiobitrate)
 
-            biglist=[]
-            baselist=[self.ffmpeg, "-i", str(filepath)]
-            videocodeclist=["-c:v", videocodec]
-            bitratelist=["-b:v", videobitrate]
-            passlist=["-pass", str(passno), "-passlogfile", str(filepath.with_suffix(""))]
-            listsuffix=["-hide_banner", "-y"]
+            biglist = []
+            baselist = [self.ffmpeg, "-i", str(filepath)]
+            videocodeclist = ["-c:v", videocodec]
+            bitratelist = ["-b:v", videobitrate]
+            passlist = ["-pass", str(passno), "-passlogfile", str(filepath.with_suffix(""))]
+            listsuffix = ["-hide_banner", "-y"]
 
-            biglist=biglist+baselist
+            biglist = biglist + baselist
 
             if videocodec not in self.nocodec:
-                biglist=biglist+videocodeclist
-                fr=frameratefilter()
+                biglist = biglist + videocodeclist
+                fr = frameratefilter()
                 if fr:
-                    biglist=biglist+fr
-                biglist=biglist+bitratelist
+                    biglist = biglist + fr
+                biglist = biglist + bitratelist
 
             if passmax is 2:
-                biglist=biglist+passlist
+                biglist = biglist + passlist
 
             if videocodecopts:
-                biglist=biglist+videocodecopts
+                biglist = biglist + videocodecopts
 
             if passno is 1:
-                biglist=biglist+["-an"]+listsuffix+["-f", "matroska", "/dev/null"]
+                biglist = biglist + ["-an"] + listsuffix + ["-f", "matroska", "/dev/null"]
             else:
                 if audiocodec not in (None, "none"):
-                    biglist=biglist+["-c:a"]+[audiocodec]
+                    biglist = biglist + ["-c:a"] + [audiocodec]
                     if audiocodec is not "copy":
-                        biglist=biglist+["-b:a"]+[audiobitrate]
+                        biglist = biglist + ["-b:a"] + [audiobitrate]
                         if audiocodecopts:
-                            biglist=biglist+audiocodecopts
+                            biglist = biglist + audiocodecopts
                         if audiofilteropts:
-                            biglist=biglist+["-af"]+audiofilteropts
+                            biglist = biglist + ["-af"] + audiofilteropts
                 else:
                     biglist.append("-an")
-                biglist=biglist+listsuffix+[output]
+                biglist = biglist + listsuffix + [output]
 
-
-            #print(list(flatten(biglist))) #temporary for debugging purposes
+            # print(list(flatten(biglist))) #temporary for debugging purposes
 
             return biglist
 
