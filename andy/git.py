bda36bab45e47fdc79126d60fd29ca92a0d5073f
Andrew Nelson <andrew@andrewnelson.org>
2016-05-16 16:10:55 -0700

    Add docstrings in Git class.

--- andy/git.py
+++ andy/git.py
@@ -1,3 +1,4 @@
+
 import pathlib
 from collections import deque
 
@@ -5,8 +6,18 @@ from andy.util import Color, Util, Program
 
 
 class Git:
-
+    """High level functions for working with git, using my runprogram function.
+        
+        directory is the directory to be worked in.
+        
+        use_sudo controls whether sudo is used.
+        
+        sudo_user tells runprogram what user to run sudo under.
+        
+        url tells git what url to use when cloning a repository."""
+        
     def __init__(self, directory, use_sudo=None, sudo_user=None, url=None):
+        
         self.colors = Color()
         self.util = Util()
         self.program = Program()
@@ -35,6 +46,9 @@ class Git:
         self.sudo_user = sudo[1]
 
     def clean_lock(self):
+    
+        """Cleans any stale lock files (currently only index.lock but more will be added if discovered)"""
+        
         if self.path.joinpath(".git", "index.lock").exists():
             if self.use_sudo:
                 self.program.runprogram(["rm", str(self.path.joinpath(".git", "index.lock"))], use_sudo=self.use_sudo, user=self.sudo_user)
@@ -42,16 +56,27 @@ class Git:
                 self.path.joinpath(".git", "index.lock").unlink()
 
     def clone(self):
+        
+        """Clones the repository to the directory specified by the class using the url specified by the class."""
+        
         if not self.url:
             print("{} url not defined.".format(self.colors.mood("sad")))
             raise ValueError
         self.program.runprogram(["git", "clone", self.url, self.directory], use_sudo=self.use_sudo, user=self.sudo_user)
 
     def gc(self, aggressive=False):
+    
+        """Runs git's garbage collection subcommand.
+        
+        aggressive controles whether to use aggressive mode, USE SPARINGLY, this is much slower than regular mode."""
+        
         gccmd = deque(["git", "gc"])
         if aggressive:
             gccmd.append("--aggressive")
         self.program.runprogram(gccmd, workdir=self.directory, use_sudo=self.use_sudo, user=self.sudo_user)
 
     def pull(self):
+    
+        """Updates the repository in the directory specified by the class."""
+    
         self.program.runprogram(["git", "pull"], workdir=self.directory, use_sudo=self.use_sudo, user=self.sudo_user)
