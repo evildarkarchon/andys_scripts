85a160ba81ddf1c3c94b7d73e104119b4c9bd235
Andrew Nelson <andrew@andrewnelson.org>
2016-05-16 19:53:53 -0700

    Actually make the script error out if audio codec or video codec is not in the nocodec variable, no videoinfo database has been initialized and no bitrate is specified.

--- andy/abs.py
+++ andy/abs.py
@@ -97,6 +97,13 @@ class ABS(VideoInfo, VideoUtil):
         filepath = pathlib.Path(filename).resolve()
         outpath = self.outputpath.joinpath(filepath.with_suffix(container).name)
         output = str(outpath)
+        
+        if not self.vi and videocodec not in self.nocodec and (videobitrate not in vars() or not videobitrate):
+            print('{} No videoinfo database initialized, videocodec is not None, "none", or "copy", and no bitrate specified'.format(self.colors.mood("sad")))
+            raise ValueError
+        elif not self.vi and audiocodec not in self.nocodec and (audiobitrate not in vars() or not audiobitrate):
+            print('{} No videoinfo database initialized, audiocodec is not None, "none", or "copy", and no bitrate specified'.format(self.colors.mood("sad")))
+            raise ValueError
 
         def frameratefilter():
             """Helper function to give the command list function the framerate that was either specified on the command line or from the videoinfo database."""
