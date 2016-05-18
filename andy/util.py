7f89ed29c4e478699ebbde6232ad6f82c318c109
Andrew Nelson <andrew@andrewnelson.org>
2016-05-17 18:56:38 -0700

    Add docstring to prettylist and add new datediff utility function. Put in a POSIX timestamp and it will return the difference between the current time as of when it was invoked and the timestamp that was given.

--- andy/util.py
+++ andy/util.py
@@ -15,6 +15,7 @@ except ImportError:
     pass
 
 from termcolor import colored
+from datetime import datetime
 
 
 class Color:
@@ -156,11 +157,27 @@ class Util(Color):
             hasher.update(afile.read())
         return hasher.hexdigest()
 
-    def prettylist(text, quotes=False, sep=", "):
+    def prettylist(self, text, quotes=False, sep=", "):
+        """Front-end function that takes an iterable and creates a "pretty" list from it.
+        
+        text is the iterable to be used for making the pretty list.
+        
+        quotes sets whether you want each entry to have quotes around them or not.
+        
+        sep takes a string that will be used as the separator for the list."""
+
         if quotes:
             return sep.join(repr(e) for e in text)
         else:
             return sep.join(str(e) for e in text)
+    
+    def datediff(self, timestamp):
+        """Front-end Function that will return a timedelta of the supplied timestamp vs. a snapshot of the current time.
+        
+        timestamp takes a POSIX timestamp and uses it to create a datetime object for comparison."""
+        now=datetime.now()
+        then=datetime.fromtimestamp(timestamp)
+        return now - then
 
 
 class Program(Util, Color):
