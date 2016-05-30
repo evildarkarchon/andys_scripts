7657503348a9b9cad6264cbdede60d7fba992a1d
Andrew Nelson <andrew@andrewnelson.org>
2016-05-30 15:40:53 -0700

    Implement VideoInfo::find, VideoInfo::Generate.hash (ruby version of the function that generates the dictionary in python) and VideoInfo::Generate.query (ruby version of the function that takes care of writing the info to the database)

--- andyrb/videoinfo.rb
+++ andyrb/videoinfo.rb
@@ -1,32 +1,55 @@
 require 'sqlite3'
 require 'json'
 require 'pathname'
+require 'filesize'
+require 'filemagic'
+require 'find'
 # insert documentation here
 module VideoInfo
-  CreateStatement = 'CREATE TABLE IF NOT EXISTS videoinfo (id integer primary key, filename text unique, duration text, duration_raw real, streams integer, bitrate_total text, bitrate_0 text, bitrate_1 text, bitrate_0_raw integer, bitrate_1_raw integer, codec_0 text, codec_1 text, container text, width integer, height integer, frame_rate real, hash text unique)'.freeze
+  CreateStatement = 'CREATE TABLE IF NOT EXISTS videoinfo (id integer primary key, filename text unique, duration text, duration_raw real, streams integer, bitrate_total text, bitrate_0 text, type_0 text, bitrate_1 text, bitrate_0_raw integer, bitrate_1_raw integer, type_1 text, codec_0 text, codec_1 text, container text, width integer, height integer, frame_rate real, hash text unique)'.freeze
   CreateStatementJSON = 'CREATE TABLE IF NOT EXISTS videojson (id INTEGER PRIMARY KEY, filename TEXT UNIQUE, jsondata JSON)'.freeze
+
+  def self.find(directory, printresults = false)
+    magic = FileMagic.new
+    directories = []
+    Find.find(directory) do |path|
+      result = magic.file(path) if FileTest.file?(path)
+      pathpath = Pathname.new(path)
+      directories << pathpath.dirname if result && result.to_s.include?('SQLite 3.x') && !pathpath.extname.include?('.bak') && !pathpath.extname.include?('.test')
+      # puts path if result && result.to_s.include?('SQLite 3.x database')
+    end
+    if printresults
+      directories.each do |path|
+        puts "'#{path}'"
+      end
+    end
+    directories
+  end
+
   class Database
     def initialize(dbpath = Pathname.new('./videoinfo.sqlite'))
       @db = SQLite3::Database.new(dbpath.to_s)
       @db.type_translation = true
-      @db.auto_vacuum = true if @db.auto_vacuum is false
+      @db.auto_vacuum = true unless @db.auto_vacuum
       @db.cache_size = -2000 if @db.cache_size > -2000
+      @db.execute 'vacuum'
     end
 
     def write(query, *values)
       dbvalues = values.to_a if values.responds_to?(:to_a)
-      @db.execute(SQLite3::Database.quote(query), dbvalues)
+      @db.execute(query, dbvalues)
     end
 
     def writenp(query, inputhash)
-      inputhash.to_h if inputhash.respond_to?(:to_h)
+      input = inputhash.to_h if inputhash.respond_to?(:to_h)
 
-      @db.execute(query, inputhash)
+      @db.execute(query, input)
     end
 
     def read(query, *values)
-      inputvalues = values.to_a if values.responds_to?(:to_a)
-      output = @db.execute(SQLite3::Database.quote(query), inputvalues)
+      inputvalues = values.to_a if values.respond_to?(:to_a)
+      output = @db.execute(query, inputvalues)
+      output = nil if output.empty?
       output
     end
 
@@ -47,10 +70,13 @@ module VideoInfo
     end
 
     def query(inputhash)
-      columns = inputhash.keys.join(', ')
-      placeholders = inputhash.keys.join(':' + ', :')
+      columns = inputhash[0].keys.join(', ')
+      placeholders = inputhash[0].keys.join(':' + ', :')
       viquery = "insert into videoinfo (#{columns}) values ({#{placeholders})"
-      @vi.write(viquery, inputhash)
+      jsondata = JSON.parse(inputhash[1])
+      jsondata['format']['filename'] = File.basename(jsondata['format']['filename'])
+      @vi.write(viquery, inputhash[0])
+      @vi.write('insert into videojson (filename, jsondata) values (?, ?)', jsondata['format']['filename'], inputhash[1]) if @vi.read('select filename from videojson where filename = ?', jsondata['format']['filename']).empty?
     end
 
     def json(filename)
@@ -64,14 +90,42 @@ module VideoInfo
       output
     end
 
-    def genhash(filename, inputjson, filehash)
-      jsondata = json.parse(inputjson)
+    def hash(filename, inputjson, filehash)
+      jsondata = JSON.parse(inputjson)
       filepath = Pathname.new(filename)
       outhash = {}
 
       outhash['filename'] = filepath.basename
       outhash['hash'] = filehash
       outhash['container'] = jsondata['format']['formatname']
+      outhash['duration'] = Time.at(jsondata['format']['duration']).utc.strftime('%H:%M:%S')
+      outhash['duration_raw'] = jsondata['format']['duration']
+      outhash['streams'] = jsondata['format']['nb_streams']
+
+      outhash['bitrate_0'] = Filesize.from(jsondata['streams'][0]['bit_rate']) if jsondata['streams'][0].key?('bit_rate')
+      outhash['bitrate_0'] = Filesize.from(jsondata['streams'][0]['tags']['BPS']) if jsondata['streams'][0].key?('tags') && jsondata['streams'][0]['tags'].key?('BPS')
+      outhash['bitrate_0_raw'] = jsondata['streams'][0]['bitrate'] if jsondata['streams'][0].key?('bitrate')
+      outhash['bitrate_0_raw'] = jsondata['streams'][0]['tags']['BPS'] if jsondata['streams'][0].key?('tags') && jsondata['streams'][0]['tags'].key?('BPS')
+      outhash['type_0'] = jsondata['streams'][0]['codec_type'] if jsondata['streams'][0].key?('codec_type')
+      outhash['codec_0'] = jsondata['streams'][0]['codec_name'] if jsondata['streams'][0].key?('codec_name')
+
+      outhash['bitrate_1'] = Filesize.from(jsondata['streams'][1]['bit_rate']) if jsondata['streams'].length >= 2 && jsondata['streams'][1].key?('bit_rate')
+      outhash['bitrate_1'] = Filesize.from(jsondata['streams'][1]['tags']['BPS']) if jsondata['streams'].length >= 2 && jsondata['streams'][1].key?('tags') && jsondata['streams'][1]['tags'].key?('BPS')
+      outhash['type_1'] = jsondata['streams'][1]['codec_type'] if jsondata['streams'].length >= 2 && jsondata['streams'][1].key?('codec_type')
+      outhash['codec_1'] = jsondata['streams'][1]['codec_name'] if jsondata['streams'].length >= 2 && jsondata['streams'][1].key?('codec_name')
+
+      outhash['height'] = jsondata['streams'][0]['height'] if jsondata['streams'][0]['height']
+      outhash['height'] = jsondata['streams'][1]['height'] if jsondata['streams'].length >= 2 && jsondata['streams'][1].key?('height')
+      outhash['width'] = jsondata['streams'][0]['width'] if jsondata['streams'][0].key?('width')
+      outhash['width'] = jsondata['streams'][1]['width'] if jsondata['streams'].length >= 2 && jsondata['streams'][1].key?('width')
+
+      outhash['frame_rate'] = nil
+      begin
+        outhash['frame_rate'] = jsondata['streams'][0]['frame_rate'].to_f if jsondata['streams'][0].key?('frame_rate') && jsondata['streams'][0]['frame_rate'].responds_to(:to_f) && jsondata['streams'][0]['frame_rate'].to_f >= 1.0
+        outhash['frame_rate'] = jsondata['streams'][1]['frame_rate'].to_f if jsondata['streams'].length >= 2 && jsondata['streams'][1].key?('frame_rate') && jsondata['streams'][1]['frame_rate'].to_f >= 1.0
+      rescue ZeroDivisionError
+        outhash['frame_rate'] = nil
+      end
 
       return outhash, inputjson # rubocop:disable Style/RedundantReturn
     end
