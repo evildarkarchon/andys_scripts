cce3f26e41ad9f7bce2d371015ce89bc8d32d737
Andrew Nelson <andrew@andrewnelson.org>
2016-06-01 03:50:43 -0700

    Add some initial require statements to the ruby scripts

---
+++ absconvert.rb
@@ -0,0 +1,6 @@
+require 'ostruct'
+require 'optparse'
+require 'pathname'
+
+require_relative 'andyrb/mood'
+require_relative 'andyrb/util'
---
+++ ttrss_update.rb
@@ -0,0 +1,8 @@
+require 'ostruct'
+require 'optparse'
+require 'pathname'
+
+require_relative 'andyrb/mood'
+require_relative 'andyrb/util'
+require_relative 'andyrb/git'
+
