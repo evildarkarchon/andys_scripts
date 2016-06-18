952354b37c3d28431008a449077712559e90e16a
Andrew Nelson <andrew@andrewnelson.org>
2016-06-18 03:42:17 -0700

    All mood methods use blocks now so that way I can call it once even for situations where I would traditionally call it multiple times for different conditions. Also, added a new mood called "colorful" which is a catchall so that you can specify any color that termcolor will accept and it will do what Mood has always done, just with the new color.

--- andyrb/mood.rb
+++ andyrb/mood.rb
@@ -1,15 +1,20 @@
 require 'termcolor'
 # Convenience methods to add a colored star based on the "mood" method called.
 module Mood
-  def self.happy(inputstring)
-    ['<green>*</green>', inputstring].join(' ').termcolor
+  def self.happy
+    "<green>*</green> #{yield}".termcolor
   end
 
-  def self.neutral(inputstring)
-    ['<yellow>*</yellow>', inputstring].join(' ').termcolor
+  def self.neutral
+    "<yellow>*</yellow> #{yield}".termcolor
   end
 
-  def self.sad(inputstring)
-    ['<red>*</red>', inputstring].join(' ').termcolor
+  def self.sad
+    "<red>*</red> #{yield}".termcolor
+  end
+
+  def self.colorful(color)
+    colorful = "<#{color}>*</#{color}>".termcolor
+    yield colorful
   end
 end
