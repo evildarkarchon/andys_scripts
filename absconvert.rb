7230465f08ba428ed1cfda3185ebeda6b5163131
Andrew Nelson <andrew@andrewnelson.org>
2016-06-19 23:25:50 -0700

    Remove some blank lines from the option block, make script error out if passes option is not 1 or 2.

--- absconvert.rb
+++ absconvert.rb
@@ -35,36 +35,23 @@ class Options
 
     optparse = OptionParser.new do |opts|
       opts.on('--video-codec [codec]', 'Video codec for the output video.') { |vcodec| options.videocodec = vcodec }
-
       opts.on('--video-bitrate [bitrate]', 'Video bitrate for the output video.') { |vbitrate| options.videobitrate = vbitrate }
-
       opts.on('--passes [pass]', '-p [pass]', 'Number of video encoding passes.') do |passes|
         raise ArgumentError unless passes.respond_to?(:to_i)
+        raise ArgumentError unless passes.between?(1, 2)
         options.passes = passes.to_i
       end
-
       opts.on('--frame-rate [rate]', '-f [rate]', 'Manually specify the frame rate (e.g. useful for MPEG2-PS files)') { |fr| options.framerate = fr.to_f if fr.respond_to?(:to_f) }
-
       opts.on('--audio-bitrate [bitrate]', 'Bitrate of the output audio.') { |abitrate| options.audiobitrate = abitrate }
-
       opts.on('--audio-codec [codec]', 'Codec of the output audio.') { |acodec| options.audiocodec = acodec }
-
       opts.on('--audio-filter [filter]', 'Filter to be used for the output audio.') { |afilter| options.audiofilter = afilter }
-
       opts.on('--database [location]', 'Location of the videoinfo database') { |db| options.dbpath = Pathname.new(db) }
-
       opts.on('--convert-test', "Don't delete any videoinfo entries.") { |ct| options.converttest = ct }
-
       opts.on('--config [path]', '-c [path]', 'Location of the configuration json file') { |config| options.config = Pathname.new(config) }
-
       opts.on('--container [extension]', 'Container to be used for the output file (the dot must be included).') { |ext| options.container = ext }
-
       opts.on('--no-sort', "Don't sort the file list.") { options.sort = false }
-
       opts.on('--debug', '-d', 'Print variables and exit.') { options.debug = true }
-
       opts.on('--backup [dir]', '-b [dir]', 'Location of the backup directory (if any)') { |backup| options.backup = Pathname.new(backup) }
-
       opts.on('--output [dir]', '-o [dir]', 'Location of the output directory') { |output| options.outputdir = Pathname.new(output) }
     end
     optparse.parse!(args)
