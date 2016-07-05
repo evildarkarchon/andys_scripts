4a27afb4a2789be285a2ccc2bba13de203c7e0a9
Andrew Nelson <andrew@andrewnelson.org>
2016-07-04 21:04:57 -0700

    (Theoretically) make it so that mood can be used both with and without a code block.

--- andyrb/mood.rb
+++ andyrb/mood.rb
@@ -1,16 +1,25 @@
 require 'termcolor'
 # Convenience methods to add a colored star based on the "mood" method called.
 module Mood
-  def self.happy
-    "<green>*</green> #{yield}".termcolor
+  def self.happy(message = nil)
+    out = nil
+    out = "<green>*</green> #{message}".termcolor unless block_given?
+    out = "<green>*</green> #{yield}".termcolor if block_given?
+    out
   end
 
-  def self.neutral
-    "<yellow>*</yellow> #{yield}".termcolor
+  def self.neutral(message = nil)
+    out = nil
+    out = "<yellow>*</yellow> #{message}".termcolor unless block_given?
+    out = "<yellow>*</yellow> #{yield}".termcolor if block_given?
+    out
   end
 
-  def self.sad
-    "<red>*</red> #{yield}".termcolor
+  def self.sad(message = nil)
+    out = nil
+    out = "<red>*</red> #{message}".termcolor unless block_given?
+    out = "<red>*</red> #{yield}".termcolor if block_given?
+    out
   end
 
   def self.colorful(color)
