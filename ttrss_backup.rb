c44603bf796cc7ce45ebb538a2063589db816bab
Andrew Nelson <andrew@andrewnelson.org>
2016-07-04 21:07:59 -0700

    Add outline for what work needs to be done for ttrss_backup.rb

--- ttrss_backup.rb
+++ ttrss_backup.rb
@@ -4,3 +4,17 @@ require 'pathname'
 
 require_relative 'andyrb/mood'
 require_relative 'andyrb/util'
+
+# insert app location code here.
+
+# insert option parsing code here.
+
+# insert database name variable and filename code here.
+
+# insert begin block with code to dump database and compress the output.
+
+# pgdump command line = "-C", "-F", "t", "-c", "--if-exists", "-d", database, "-U", "postgres"
+
+# raise Interrupt for ctrl-C
+
+# insert cleanup code here.
