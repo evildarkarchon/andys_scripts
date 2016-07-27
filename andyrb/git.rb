76ea893cd808c0837e7385555164eca95b1da000
Andrew Nelson <andrew@andrewnelson.org>
2016-07-26 20:50:21 -0700

    Change the attr_accessors into attr_readers and move the declaration to the right place.

--- andyrb/git.rb
+++ andyrb/git.rb
@@ -5,11 +5,11 @@ require 'pathname'
 require_relative 'mood'
 require_relative 'util'
 class Git
+  attr_reader :use_sudo, :sudo_user, :wd
   def initialize(wd, use_sudo = false, sudo_user = 'root')
     @use_sudo = use_sudo
     @sudo_user = sudo_user
     @wd = wd
-    attr_accessor :use_sudo, :sudo_user, :wd
     if !@use_sudo && !Util.privileged?(@sudo_user)
       @use_sudo = true
       puts Mood.neutral { 'use_sudo was not set properly, defaulting to root, fix the code asap.' }
