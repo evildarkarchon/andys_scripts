1a02d246c5bc9f8c4650ce8289930b19f06e25ad
Andrew Nelson <andrew@andrewnelson.org>
2016-12-12 00:48:10 -0800

    remove the ttrss script ruby versions, i'm probably not going to port them to ruby.

--- ttrss_cleanup.rb
+++
@@ -1,6 +0,0 @@
-require 'ostruct'
-require 'optparse'
-require 'pathname'
-
-require_relative 'andyrb/mood'
-require_relative 'andyrb/util'
--- ttrss_update.rb
+++
@@ -1,8 +0,0 @@
-require 'ostruct'
-require 'optparse'
-require 'pathname'
-
-require_relative 'andyrb/mood'
-require_relative 'andyrb/util'
-require_relative 'andyrb/git'
-
