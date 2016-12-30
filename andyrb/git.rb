8e9f0faa4e7b9a6044477ddc61d392668af1227f
Andrew Nelson <andrew@andrewnelson.org>
2016-12-30 03:55:15 -0800

    Bring git.rb in line with git.py (hopefully)

--- andyrb/git.rb
+++ andyrb/git.rb
@@ -6,30 +6,61 @@ require_relative 'mood'
 require_relative 'util'
 class Git
   attr_reader :use_sudo, :sudo_user, :wd, :wdpath
-  attr_accessor :aggressive
   def initialize(wd, use_sudo = false, sudo_user = 'root')
     @use_sudo = use_sudo
     @sudo_user = sudo_user
     @wdpath = Pathname.new(wd)
-    @aggressive = false
     @wd = wd
-    wdlock = @wdpath.join('.git/index.lock')
+    @wdlock = @wdpath.join('.git/index.lock')
 
-    if !@use_sudo && !Util.privileged?(@sudo_user)
+    case
+    when !@use_sudo && !Util.privileged?(@sudo_user)
       @use_sudo = true
-      puts Mood.neutral { 'use_sudo was not set properly, defaulting to root, fix the code asap.' }
-    end
-
-    if @use_sudo && !@sudo_user
+      puts Mood.neutral('use_sudo was not set properly, defaulting to root, fix the code asap.')
+    when @use_sudo && !@sudo_user
       @sudo_user = 'root'
-      puts Mood.neutral { 'sudo_user was not set properly, defaulting to root, fix the code asap.' }
+      puts Mood.neutral('sudo_user was not set properly, defaulting to root, fix the code asap.')
+    when !@wdpath.writable? && !@use_sudo
+      @use_sudo = true
+      @sudo_user = 'root'
+      puts Mood.neutral('Working directory is not writable, sudo forced.')
     end
   end
 
   def clean_lock
-    if wdlock.exist?
-      wdlock.delete unless @use_sudo
-      Util::Program.runprogram(['sudo', '-u', @sudo_user, 'rm', wdlock.to_s]) if @use_sudo
+    # @wdlock.delete unless @use_sudo && @wdlock.exist?
+    # Util::Program.runprogram(['sudo', '-u', @sudo_user, 'rm', @wdlock.to_s]) if @use_sudo && @wdlock.exist?
+    case
+    when !@use_sudo && @wdlock.exist?
+      @wdlock.delete
+    when @use_sudo && @wdlock.exist?
+      Util::Program.runprogram(%W(sudo -u #{@sudo_user} rm #{@wdlock}))
+    end
+  end
+
+  def clone(url)
+    # Util::Program.runprogram(['git', 'clone', url, @wd]) unless @use_sudo
+    # Util::Program.runprogram(['sudo', '-u', @sudo_user, 'git', 'clone', url, @wd]) if @use_sudo
+    case
+    when @use_sudo
+      Util::Program.runprogram(%W(sudo -u #{@sudo_user} git clone #{url} #{@wd}))
+    else
+      Util::Program.runprogram(%W(git cline #{url} #{@wd}))
+    end
+  end
+
+  def gc(aggressive = false)
+    case
+    when !aggressive && !@use_sudo
+      Util::Program.runprogram(%w(git gc))
+    when aggressive && !@use_sudo
+      Util::Program.runprogram(%w(git gc --aggressive))
+    when @use_sudo && !aggressive
+      # Util::Program.runprogram(['sudo', '-u', @sudo_user, 'git', 'gc'])
+      Util::Program.runprogram(%W(sudo -u #{@sudo_user} git gc))
+    when @use_sudo && aggressive
+      # Util::Program.runprogram(['sudo', '-u', @sudo_user, 'git', 'gc', '--aggressive'])
+      Util::Program.runprogram(%W(sudo -u #{@sudo_user} git gc --aggressive))
     end
   end
 end
