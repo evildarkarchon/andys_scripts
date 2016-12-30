bff1bb48e6fc2c727ad22b79c63c5137b24260c7
Andrew Nelson <andrew@andrewnelson.org>
2016-12-30 03:57:44 -0800

    Add recursive_symbolize_keys to Util, much thanks to Stack Overflow for the snippet.

--- andyrb/util.rb
+++ andyrb/util.rb
@@ -35,6 +35,21 @@ module Util
     yield kwargs unless !defined?(kwargs) || kwargs.nil? || kwargs.empty?
     yield if kwargs.empty? && args.empty?
   end
+
+  def self.recursive_symbolize_keys(my_hash)
+    case my_hash
+    when Hash
+      Hash[
+        my_hash.map do |key, value|
+          [key.respond_to?(:to_sym) ? key.to_sym : key, recursive_symbolize_keys(value)]
+        end
+      ]
+    when Enumerable
+      my_hash.map { |value| recursive_symbolize_keys(value) }
+    else
+      my_hash
+    end
+  end
   # Convenience class for writing or printing pretty JSON.
   class GenJSON
     attr_reader :output
