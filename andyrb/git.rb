14a2a5ed7d633d404af1e924633f36f508753f71
Andrew Nelson <andrew@andrewnelson.org>
2017-01-14 21:34:02 -0800

    Convert to using Util::Program.runprogram's use_sudo and sudo_user arguments instead of invoking sudo ourselves. Switch some guard clauses to case statements and remove an unnecessary case statement

--- andyrb/git.rb
+++ andyrb/git.rb
@@ -11,16 +11,16 @@ class Git
     @sudo_user = sudo_user
     @wdpath = Pathname.new(wd)
     @wd = wd
-    @wdlock = @wdpath.join('.git/index.lock')
+    @wdlock = @wdpath + '.git/index.lock'
 
     case
-    when !@use_sudo && !Util.privileged?(@sudo_user)
+    when !@use_sudo && @sudo_user && !Util.privileged?(@sudo_user)
       @use_sudo = true
       puts Mood.neutral('use_sudo was not set properly, defaulting to root, fix the code asap.')
     when @use_sudo && !@sudo_user
       @sudo_user = 'root'
       puts Mood.neutral('sudo_user was not set properly, defaulting to root, fix the code asap.')
-    when !@wdpath.writable? && !@use_sudo
+    when !@wdpath.parent.writable? && !@wdpath.writable? && !@use_sudo
       @use_sudo = true
       @sudo_user = 'root'
       puts Mood.neutral('Working directory is not writable, sudo forced.')
@@ -28,49 +28,29 @@ class Git
   end
 
   def clean_lock
-    # @wdlock.delete unless @use_sudo && @wdlock.exist?
-    # Util::Program.runprogram(['sudo', '-u', @sudo_user, 'rm', @wdlock.to_s]) if @use_sudo && @wdlock.exist?
     case
+    when @use_sudo && @wdlock.exist?
+      Util::Program.runprogram(%W(rm #{@wdlock}), use_sudo: true, sudo_user: @sudo_user)
     when !@use_sudo && @wdlock.exist?
       @wdlock.delete
-    when @use_sudo && @wdlock.exist?
-      Util::Program.runprogram(%W(sudo -u #{@sudo_user} rm #{@wdlock}))
     end
   end
 
   def clone(url)
-    # Util::Program.runprogram(['git', 'clone', url, @wd]) unless @use_sudo
-    # Util::Program.runprogram(['sudo', '-u', @sudo_user, 'git', 'clone', url, @wd]) if @use_sudo
-    case
-    when @use_sudo
-      Util::Program.runprogram(%W(sudo -u #{@sudo_user} git clone #{url} #{@wd}))
-    else
-      Util::Program.runprogram(%W(git clone #{url} #{@wd}))
-    end
+    Util::Program.runprogram(%W(git clone #{url} #{@wd}), use_sudo: @use_sudo, sudo_user: @sudo_user)
   end
 
   def gc(aggressive = false)
     case
-    when !aggressive && !@use_sudo
-      Util::Program.runprogram(%w(git gc))
-    when aggressive && !@use_sudo
-      Util::Program.runprogram(%w(git gc --aggressive))
-    when @use_sudo && !aggressive
-      # Util::Program.runprogram(['sudo', '-u', @sudo_user, 'git', 'gc'])
-      Util::Program.runprogram(%W(sudo -u #{@sudo_user} git gc))
-    when @use_sudo && aggressive
-      # Util::Program.runprogram(['sudo', '-u', @sudo_user, 'git', 'gc', '--aggressive'])
-      Util::Program.runprogram(%W(sudo -u #{@sudo_user} git gc --aggressive))
+    when aggressive
+      Util::Program.runprogram(%w(git gc --aggressive), use_sudo: @use_sudo, sudo_user: @sudo_user)
+    else
+      Util::Program.runprogram(%w(git gc), use_sudo: @use_sudo, sudo_user: @sudo_user)
     end
   end
 
   def pull
     Dir.chdir(@wd)
-    case
-    when @use_sudo
-      Util::Program.runprogram(%W(sudo -u #{@sudo_user} git pull))
-    else
-      Util::Program.runprogram(%w(git pull))
-    end
+    Util::Program.runprogram(%w(git pull), use_sudo: @use_sudo, sudo_user: @sudo_user)
   end
 end
