86466aba37b15a8b5ae96b2e8919bfa355c8a65b
Andrew Nelson <andrew@andrewnelson.org>
2017-01-19 04:13:30 -0800

    Simplify the code, removing the variable entirely (was unnecessary) and put the yield statement in the message variable instead of having two different invocations based on if there's a block or not. If there's a message in the method argument, any code block will be ignored.

--- andyrb/mood.rb
+++ andyrb/mood.rb
@@ -2,30 +2,22 @@ require 'termcolor'
 # Convenience methods to add a colored star based on the "mood" method called.
 module Mood
   def self.happy(message = nil)
-    out = nil
-    out = "<green>*</green> #{message}".termcolor unless block_given?
-    out = "<green>*</green> #{yield}".termcolor if block_given?
-    out
+    message = yield if block_given? && message.nil?
+    "<green>*</green> #{message}".termcolor
   end
 
   def self.neutral(message = nil)
-    out = nil
-    out = "<yellow>*</yellow> #{message}".termcolor unless block_given?
-    out = "<yellow>*</yellow> #{yield}".termcolor if block_given?
-    out
+    message = yield if block_given? && message.nil?
+    "<yellow>*</yellow> #{message}".termcolor
   end
 
   def self.sad(message = nil)
-    out = nil
-    out = "<red>*</red> #{message}".termcolor unless block_given?
-    out = "<red>*</red> #{yield}".termcolor if block_given?
-    out
+    message = yield if block_given? && message.nil?
+    "<red>*</red> #{message}".termcolor
   end
 
   def self.colorful(color, message = nil)
-    colorful = nil
-    colorful = "<#{color}>*</#{color}> #{message}".termcolor unless block_given?
-    colorful = "<#{color}>*</#{color}> #{yield}".termcolor if block_given?
-    colorful
+    message = yield if block_given? && !message
+    "<#{color}>*<#{color}> #{message}".termcolor
   end
 end
