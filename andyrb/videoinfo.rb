3e91439ab872a796295c7a0df51a52c8118f606c
Andrew Nelson <andrew@andrewnelson.org>
2017-01-29 03:40:03 -0800

    Reorganize methods so that the module methods are all on top.
    Properly handle a pre-parsed hash in GenerateVideoInfo.genhash
    
    Change the frame rate calculation to use a case statement and have the calculator invocation be a lambda function since it was the same call just different sources.
    
    Change the respond_to? queries to add the question mark for :key? in bitrate_1_raw
    
    Remove unnecessary code block.
    
    Change the filelist whitelist to simply concatenate the two lists together, that's really all that needs to be done.

--- andyrb/videoinfo.rb
+++ andyrb/videoinfo.rb
@@ -25,6 +25,151 @@ module GenerateVideoInfo
     out
   end
 
+  def self.genhash(filename, inputjson, filehash = nil)
+    raise 'You must supply either a json string or a pre-parsed hash.' unless inputjson.is_a?(String) || inputjson.is_a?(Hash) || inputjson.is_a?(DataMapper::Collection)
+    jsondata = Util.recursive_symbolize_keys(JSON.parse(inputjson)) if inputjson.is_a? String
+    jsondata = Util.recursive_symbolize_keys(JSON.parse(inputjson[0][:jsondata])) if inputjson.is_a? DataMapper::Collection
+    jsondata = Util.recursive_symbolize_keys(inputjson) if inputjson.is_a? Hash
+    # jsondata = Util.recursive_symbolize_keys(jsondata)
+    # puts jsondata
+    filepath = Pathname.new(filename)
+    calc = Dentaku::Calculator.new
+    outhash = {}
+    outhash[:filename] = filepath.basename.to_s
+    outhash[:filehash] = filehash[filepath.realpath.to_s] if filehash
+    outhash[:container] = jsondata[:format][:format_name]
+    outhash[:duration] = Time.at(jsondata[:format][:duration].to_f).utc.strftime('%H:%M:%S')
+    outhash[:duration_raw] = jsondata[:format][:duration]
+    outhash[:numstreams] = jsondata[:format][:nb_streams]
+
+    outhash[:bitrate_total] = case
+    when jsondata[:format][:bit_rate].to_i >= 1_000_000
+      Filesize.from(jsondata[:format]['bit_rate'].to_s + 'b').to('Mb').round(2).to_s + 'Mb/s' if jsondata[:format].key?('bit_rate')
+      when jsondata[:format][:bit_rate].to_i.between?(1000, 999_999)
+        Filesize.from(jsondata[:format]['bit_rate'].to_s + 'b').to('Kb').round.to_s + 'Kb/s'
+      when jsondata[:format][:bit_rate].to_i < 1000
+        jsondata[:format][:bit_rate].to_s + 'b/s'
+      end # rubocop:disable Lint/EndAlignment
+
+    outhash[:bitrate_0_raw] = case
+      when jsondata[:streams][0].key?('bit_rate')
+        jsondata[:streams][0]['bit_rate']
+      when jsondata[:streams][0][:tags].key?(:BPS)
+        jsondata[:streams][0][:tags][:BPS]
+      end # rubocop:disable Lint/EndAlignment
+
+    outhash[:bitrate_0] = case
+    when outhash[:bitrate_0_raw].to_i >= 1_000_00
+      Filesize.from(outhash[:bitrate_0_raw].to_s + 'b').to('Mb').round(2).to_s + 'Mb/s'
+    when outhash[:bitrate_0_raw].to_i.between?(1000, 999_999)
+      Filesize.from(outhash[:bitrate_0_raw].to_s + 'b').to('Kb').round.to_s + 'Kb/s'
+    when outhash[:bitrate_0_raw].to_i < 1000
+      outhash[:bitrate_0_raw].to_s + 'b/s'
+    end # rubocop:disable Lint/EndAlignment
+
+    outhash[:type_0] = jsondata[:streams][0][:codec_type] if jsondata[:streams][0].key?(:codec_type)
+    outhash[:codec_0] = jsondata[:streams][0][:codec_name] if jsondata[:streams][0].key?(:codec_name)
+
+    outhash[:bitrate_1_raw] = case
+    when jsondata[:streams][1].respond_to?(:key?) && jsondata[:streams][1].key?('bit_rate')
+      jsondata[:streams][1]['bit_rate']
+    when jsondata[:streams][1].respond_to?(:key?) && jsondata[:streams][1].key?(:tags) && jsondata[:streams][1][:tags].key?(:BPS)
+      jsondata[:streams][1][:tags][:BPS]
+    end # rubocop:disable Lint/EndAlignment
+
+    outhash[:bitrate_1] = case
+    when jsondata[:streams].length >= 2 && outhash[:bitrate_1_raw].to_i >= 1_000_000
+      Filesize.from(outhash[:bitrate_1_raw].to_s + 'b').to('Mb').round(2).to_s + 'Mb/s'
+    when jsondata[:streams].length >= 2 && outhash[:bitrate_1_raw].to_i.between?(1000, 999_999)
+      Filesize.from(outhash[:bitrate_1_raw].to_s + 'b').to('Kb').round.to_s + 'Kb/s'
+    when jsondata[:streams].length >= 2 && outhash[:bitrate_1_raw].to_i < 1000
+      outhash[:bitrate_1_raw].to_s + 'b/s'
+    end # rubocop:disable Lint/EndAlignment
+
+    outhash[:type_1] = jsondata[:streams][1][:codec_type] if jsondata[:streams].length >= 2 && jsondata[:streams][1].respond_to?(:key?) && jsondata[:streams][1].key?(:codec_type)
+
+    outhash[:codec_1] = jsondata[:streams][1][:codec_name] if jsondata[:streams].length >= 2 && jsondata[:streams][1].respond_to?(:key?) && jsondata[:streams][1].key?(:codec_name)
+
+    outhash[:height] = case
+    when jsondata[:streams][0][:height]
+      jsondata[:streams][0][:height]
+    when jsondata[:streams].length >= 2 && jsondata[:streams][1].respond_to?(:key) && jsondata[:streams][1].key?(:height)
+      jsondata[:streams][1][:height]
+    end # rubocop:disable Lint/EndAlignment
+
+    outhash[:width] = case
+    when jsondata[:streams][0].key?(:width)
+      jsondata[:streams][0][:width]
+    when jsondata[:streams].length >= 2 && jsondata[:streams][1].respond_to?(:key) && jsondata[:streams][1].key?(:width)
+      jsondata[:streams][1][:width]
+    end # rubocop:disable Lint/EndAlignment
+
+    begin
+      fr = ->(n) { calc.evaluate(n).to_f.round(2) }
+      outhash[:frame_rate] = case # Used 4 'when' statements for readability
+      when jsondata.respond_to?(:dig) && jsondata.dig(:streams, 0, :avg_frame_rate)
+        fr.call(jsondata[:streams][0][:avg_frame_rate])
+      when !jsondata.respond_to?(:dig) && jsondata.key?(:streams) && jsondata[:streams].key?(0) && jsondata[:streams][0].key?(:avg_frame_rate)
+        fr.call(jsondata[:streams][0][:avg_frame_rate])
+      when jsondata.respond_to?(:dig) && jsondata.dig(:streams, 1, :avg_frame_rate)
+        fr.call(jsondata[:streams][1][:avg_frame_rate])
+      when !jsondata.respond_to?(:dig) && !jsondata[:streams][0].key?(:avg_frame_rate) && jsondata[:streams].length >= 2 && jsondata[:streams][1].key?(:avg_frame_rate)
+        fr.call(jsondata[:streams][1][:avg_frame_rate])
+      end # rubocop:disable Ling/EndAlignment
+    rescue ZeroDivisionError
+      outhash[:frame_rate] = nil
+    end
+    yield outhash if block_given?
+    outhash
+  end
+
+  def self.genfilelist(filelist, testmode = false, sort = true)
+    wl = lambda do
+      whl = %w(video/x-flv video/mp4 video/mp2t video/3gpp video/quicktime video/x-msvideo video/x-ms-wmv video/webm video/x-matroska video/3gpp2 audio/x-wav)
+      whl2 = %w(audio/wave video/dvd video/mpeg application/vnd.rn-realmedia-vbr audio/vnd.rn-realaudio audio/x-realaudio)
+      whl += whl2
+      whl
+    end
+    whitelist = wl.call
+    magic = FileMagic.new(:mime_type)
+    filelist = Util::SortEntries.sort(filelist) if sort
+    puts 'Files to be examined:' if testmode
+    outlist = []
+    filelist.each do |entry|
+      if testmode
+        puts magic.flags
+        puts magic.file(entry)
+      end
+      outlist << entry if whitelist.include?(magic.file(entry)) && !testmode
+      puts Mood.happy { entry } if whitelist.include?(magic.file(entry)) && testmode
+    end
+    magic.close
+    outlist = Util::SortEntries.sort(outlist) if sort
+    yield outlist if block_given?
+    outlist
+  end
+
+  def self.find(directory, verbose = false)
+    magic = FileMagic.new
+    dirpath = Pathname.new(directory)
+    blacklist = /jpg|gif|png|flv|mp4|mkv|webm|vob|ogg|drc|avi|wmv|yuv|rm|rmvb|asf|m4v|mpg|mp2|mpeg|mpe|mpv|3gp|3g2|mxf|roq|nsv|f4v|wav|ra|mka|pdf|odt|docx|webp|swf|cb7|zip|7z|xml|log/i
+    initdirectories = dirpath.find.to_a
+    initdirectories.keep_if(&:file?)
+    initdirectories.delete_if { |i| i.extname =~ blacklist }
+    initdirectories.keep_if do |i|
+      result = magic.file(i.to_s)
+      result.include?('SQLite 3.x')
+    end
+    directories = []
+    initdirectories.each do |i|
+      puts Mood.happy { "Adding #{i.dirname} to list" } if verbose
+      directories << i.dirname
+    end
+    directories.uniq!
+    yield directories if block_given?
+    directories
+  end
+
   class Videoinfo
     include DataMapper::Resource
 
@@ -112,152 +257,5 @@ module GenerateVideoInfo
       end
       out
     end
-  end
-
-  def self.genhash(filename, inputjson, filehash = nil)
-    jsondata = Util.recursive_symbolize_keys(JSON.parse(inputjson)) if inputjson.is_a? String
-    jsondata = Util.recursive_symbolize_keys(JSON.parse(inputjson[0][:jsondata])) if inputjson.is_a? DataMapper::Collection
-    # jsondata = Util.recursive_symbolize_keys(jsondata)
-    filepath = Pathname.new(filename)
-    calc = Dentaku::Calculator.new
-    outhash = {}
-    outhash[:filename] = filepath.basename.to_s
-    outhash[:filehash] = filehash[filepath.realpath.to_s] if filehash
-    outhash[:container] = jsondata[:format][:format_name]
-    outhash[:duration] = Time.at(jsondata[:format][:duration].to_f).utc.strftime('%H:%M:%S')
-    outhash[:duration_raw] = jsondata[:format][:duration]
-    outhash[:numstreams] = jsondata[:format][:nb_streams]
-
-    outhash[:bitrate_total] = case
-    when jsondata[:format][:bit_rate].to_i >= 1_000_000
-      Filesize.from(jsondata[:format]['bit_rate'].to_s + 'b').to('Mb').round(2).to_s + 'Mb/s' if jsondata[:format].key?('bit_rate')
-      when jsondata[:format][:bit_rate].to_i.between?(1000, 999_999)
-        Filesize.from(jsondata[:format]['bit_rate'].to_s + 'b').to('Kb').round.to_s + 'Kb/s'
-      when jsondata[:format][:bit_rate].to_i < 1000
-        jsondata[:format][:bit_rate].to_s + 'b/s'
-      end # rubocop:disable Lint/EndAlignment
-
-    outhash[:bitrate_0_raw] = case
-      when jsondata[:streams][0].key?('bit_rate')
-        jsondata[:streams][0]['bit_rate']
-      when jsondata[:streams][0][:tags].key?(:BPS)
-        jsondata[:streams][0][:tags][:BPS]
-      end # rubocop:disable Lint/EndAlignment
-
-    outhash[:bitrate_0] = case
-    when outhash[:bitrate_0_raw].to_i >= 1_000_00
-      Filesize.from(outhash[:bitrate_0_raw].to_s + 'b').to('Mb').round(2).to_s + 'Mb/s'
-    when outhash[:bitrate_0_raw].to_i.between?(1000, 999_999)
-      Filesize.from(outhash[:bitrate_0_raw].to_s + 'b').to('Kb').round.to_s + 'Kb/s'
-    when outhash[:bitrate_0_raw].to_i < 1000
-      outhash[:bitrate_0_raw].to_s + 'b/s'
-    end # rubocop:disable Lint/EndAlignment
-
-    outhash[:type_0] = jsondata[:streams][0][:codec_type] if jsondata[:streams][0].key?(:codec_type)
-    outhash[:codec_0] = jsondata[:streams][0][:codec_name] if jsondata[:streams][0].key?(:codec_name)
-
-    outhash[:bitrate_1_raw] = case
-    when jsondata[:streams][1].respond_to?(:key) && jsondata[:streams][1].key?('bit_rate')
-      jsondata[:streams][1]['bit_rate']
-    when jsondata[:streams][1].respond_to?(:key) && jsondata[:streams][1].key?(:tags) && jsondata[:streams][1][:tags].key?(:BPS)
-      jsondata[:streams][1][:tags][:BPS]
-    end # rubocop:disable Lint/EndAlignment
-
-    outhash[:bitrate_1] = case
-    when jsondata[:streams].length >= 2 && outhash[:bitrate_1_raw].to_i >= 1_000_000
-      Filesize.from(outhash[:bitrate_1_raw].to_s + 'b').to('Mb').round(2).to_s + 'Mb/s'
-    when jsondata[:streams].length >= 2 && outhash[:bitrate_1_raw].to_i.between?(1000, 999_999)
-      Filesize.from(outhash[:bitrate_1_raw].to_s + 'b').to('Kb').round.to_s + 'Kb/s'
-    when jsondata[:streams].length >= 2 && outhash[:bitrate_1_raw].to_i < 1000
-      outhash[:bitrate_1_raw].to_s + 'b/s'
-    end # rubocop:disable Lint/EndAlignment
-
-    outhash[:type_1] = jsondata[:streams][1][:codec_type] if jsondata[:streams].length >= 2 && jsondata[:streams][1].respond_to?(:key) && jsondata[:streams][1].key?(:codec_type)
-
-    outhash[:codec_1] = jsondata[:streams][1][:codec_name] if jsondata[:streams].length >= 2 && jsondata[:streams][1].respond_to?(:key) && jsondata[:streams][1].key?(:codec_name)
-
-    outhash[:height] = case
-    when jsondata[:streams][0][:height]
-      jsondata[:streams][0][:height]
-    when jsondata[:streams].length >= 2 && jsondata[:streams][1].respond_to?(:key) && jsondata[:streams][1].key?(:height)
-      jsondata[:streams][1][:height]
-    end # rubocop:disable Lint/EndAlignment
-
-    outhash[:width] = case
-    when jsondata[:streams][0].key?(:width)
-      jsondata[:streams][0][:width]
-    when jsondata[:streams].length >= 2 && jsondata[:streams][1].respond_to?(:key) && jsondata[:streams][1].key?(:width)
-      jsondata[:streams][1][:width]
-    end # rubocop:disable Lint/EndAlignment
-
-    outhash[:frame_rate] = nil
-    begin
-      case # Used 4 'when' statements for readability
-      when jsondata.respond_to?(:dig) && jsondata.dig(:streams, 0, :avg_frame_rate)
-        outhash[:frame_rate] = calc.evaluate(jsondata[:streams][0][:avg_frame_rate]).to_f.round(2)
-      when jsondata.respond_to?(:dig) && jsondata.dig(:streams, 1, :avg_frame_rate)
-        outhash[:frame_rate] = calc.evaluate(jsondata[:streams][1][:avg_frame_rate]).to_f.round(2)
-      when !jsondata.respond_to?(:dig) && !jsondata[:streams][0].key?(:avg_frame_rate) && jsondata[:streams].length >= 2 && jsondata[:streams][1].key?(:avg_frame_rate)
-        outhash[:frame_rate] = calc.evaluate(jsondata[:streams][1][:avg_frame_rate]).to_f.round(2)
-      when !jsondata.respond_to?(:dig) && jsondata[:streams][0].key?(:avg_frame_rate)
-        outhash[:frame_rate] = calc.evaluate(jsondata[:streams][0][:avg_frame_rate]).to_f.round(2)
-      end
-    rescue ZeroDivisionError
-      outhash[:frame_rate] = nil
-    end
-    yield outhash if block_given?
-    outhash
-  end
-
-  def self.genfilelist(filelist, testmode = false, sort = true)
-    whitelist = Util.block do
-      # wl1 = ['video/x-flv', 'video/mp4', 'video/mp2t', 'video/3gpp', 'video/quicktime', 'video/x-msvideo', 'video/x-ms-wmv', 'video/webm', 'video/x-matroska']
-      wl1 = %w(video/x-flv video/mp4 video/mp2t video/3gpp video/quicktime video/x-msvideo video/x-ms-wmv video/webm video/x-matroska video/3gpp2 audio/x-wav)
-      # wl2 = ['video/3gpp2', 'audio/x-wav', 'audio/wave', 'video/dvd', 'video/mpeg', 'application/vnd.rn-realmedia-vbr', 'audio/vnd.rn-realaudio', 'audio/x-realaudio']
-      wl2 = %w(audio/wave video/dvd video/mpeg application/vnd.rn-realmedia-vbr audio/vnd.rn-realaudio audio/x-realaudio)
-      wl1.concat(wl2)
-      wl1
-    end
-    magic = FileMagic.new(:mime_type)
-    filelist = Util::SortEntries.sort(filelist) if sort
-    puts 'Files to be examined:' if testmode
-    outlist = []
-    filelist.each do |entry|
-      if testmode
-        puts magic.flags
-        puts magic.file(entry)
-      end
-      outlist << entry if whitelist.include?(magic.file(entry)) && !testmode
-      puts Mood.happy { entry } if whitelist.include?(magic.file(entry)) && testmode
-    end
-    magic.close
-    outlist = Util.block do
-      out = Util::SortEntries.sort(outlist) if sort
-      out = outlist unless sort
-      out
-    end
-    yield outlist if block_given?
-    outlist
-  end
-
-  def self.find(directory, verbose = false)
-    magic = FileMagic.new
-    dirpath = Pathname.new(directory)
-    blacklist = /jpg|gif|png|flv|mp4|mkv|webm|vob|ogg|drc|avi|wmv|yuv|rm|rmvb|asf|m4v|mpg|mp2|mpeg|mpe|mpv|3gp|3g2|mxf|roq|nsv|f4v|wav|ra|mka|pdf|odt|docx|webp|swf|cb7|zip|7z|xml|log/i
-    initdirectories = dirpath.find.to_a
-    initdirectories.keep_if(&:file?)
-    initdirectories.delete_if { |i| i.extname =~ blacklist }
-    initdirectories.keep_if do |i|
-      result = magic.file(i.to_s)
-      result.include?('SQLite 3.x')
-    end
-    directories = []
-    initdirectories.each do |i|
-      puts Mood.happy { "Adding #{i.dirname} to list" } if verbose
-      directories << i.dirname
-    end
-    directories.uniq!
-    yield directories if block_given?
-    directories
   end
 end
