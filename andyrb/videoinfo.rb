79c71ff920f5ee9693f1c5750b0ec71c9d57a143
Andrew Nelson <andrew@andrewnelson.org>
2017-02-02 18:49:14 -0800

    Raise exception in GenerateVideoInfo.probe and GenerateVideoInfo::Data.json for when filepath is not a String or Pathname.
    Reformat the case statements.

--- andyrb/videoinfo.rb
+++ andyrb/videoinfo.rb
@@ -11,6 +11,7 @@ require_relative 'mood'
 
 module GenerateVideoInfo
   def self.probe(filepath, verbose = false)
+    raise 'First argument must be either a string or a pathname' unless filepath.is_a?(String) || filepath.is_a?(Pathname)
     out = nil
     filepath = Pathname.new(filepath) unless filepath.is_a?(Pathname)
     puts Mood.happy("Extracting metadata from #{filepath.basename}") if verbose
@@ -27,13 +28,30 @@ module GenerateVideoInfo
 
   def self.genhash(filename, inputjson, filehash = nil)
     raise 'You must supply either a json string or a pre-parsed hash.' unless inputjson.is_a?(String) || inputjson.is_a?(Hash) || inputjson.is_a?(DataMapper::Collection)
+
     jsondata = Util.recursive_symbolize_keys(JSON.parse(inputjson)) if inputjson.is_a? String
     jsondata = Util.recursive_symbolize_keys(JSON.parse(inputjson[0][:jsondata])) if inputjson.is_a? DataMapper::Collection
     jsondata = Util.recursive_symbolize_keys(inputjson) if inputjson.is_a? Hash
-    # jsondata = Util.recursive_symbolize_keys(jsondata)
-    # puts jsondata
+
     filepath = Pathname.new(filename)
+
     calc = Dentaku::Calculator.new
+
+    fs = lambda do |n|
+      out = Filesize.from(n.to_s + 'b').to('Mb').round(2).to_s + 'Mb/s' if n.to_i >= 1_000_000
+      out = Filesize.from(n.to_s + 'b').to('Kb').round.to_s + 'Kb/s' if n.to_i.between?(1000, 999_999)
+      out = n.to_s + 'b/s' if n.to_i < 1000
+      out
+    end
+
+    fr = lambda do |n|
+      begin
+        calc.evaluate(n).to_f.round(2)
+      rescue ZeroDivisionError
+        nil
+      end
+    end
+
     outhash = {}
     outhash[:filename] = filepath.basename.to_s
     outhash[:filehash] = filehash[filepath.realpath.to_s] if filehash
@@ -41,84 +59,66 @@ module GenerateVideoInfo
     outhash[:duration] = Time.at(jsondata[:format][:duration].to_f).utc.strftime('%H:%M:%S')
     outhash[:duration_raw] = jsondata[:format][:duration]
     outhash[:numstreams] = jsondata[:format][:nb_streams]
-
-    outhash[:bitrate_total] = case
-    when jsondata[:format][:bit_rate].to_i >= 1_000_000
-      Filesize.from(jsondata[:format]['bit_rate'].to_s + 'b').to('Mb').round(2).to_s + 'Mb/s' if jsondata[:format].key?('bit_rate')
-      when jsondata[:format][:bit_rate].to_i.between?(1000, 999_999)
-        Filesize.from(jsondata[:format]['bit_rate'].to_s + 'b').to('Kb').round.to_s + 'Kb/s'
-      when jsondata[:format][:bit_rate].to_i < 1000
-        jsondata[:format][:bit_rate].to_s + 'b/s'
-      end # rubocop:disable Lint/EndAlignment
-
-    outhash[:bitrate_0_raw] = case
+    outhash[:bitrate_total] = fs.call(jsondata[:format][:bit_rate])
+    outhash[:bitrate_0_raw] =
+      case
       when jsondata[:streams][0].key?('bit_rate')
         jsondata[:streams][0]['bit_rate']
       when jsondata[:streams][0][:tags].key?(:BPS)
         jsondata[:streams][0][:tags][:BPS]
-      end # rubocop:disable Lint/EndAlignment
-
-    outhash[:bitrate_0] = case
-    when outhash[:bitrate_0_raw].to_i >= 1_000_00
-      Filesize.from(outhash[:bitrate_0_raw].to_s + 'b').to('Mb').round(2).to_s + 'Mb/s'
-    when outhash[:bitrate_0_raw].to_i.between?(1000, 999_999)
-      Filesize.from(outhash[:bitrate_0_raw].to_s + 'b').to('Kb').round.to_s + 'Kb/s'
-    when outhash[:bitrate_0_raw].to_i < 1000
-      outhash[:bitrate_0_raw].to_s + 'b/s'
-    end # rubocop:disable Lint/EndAlignment
-
+      end
+    outhash[:bitrate_0] = fs.call(outhash[:bitrate_0_raw])
     outhash[:type_0] = jsondata[:streams][0][:codec_type] if jsondata[:streams][0].key?(:codec_type)
     outhash[:codec_0] = jsondata[:streams][0][:codec_name] if jsondata[:streams][0].key?(:codec_name)
+    outhash[:bitrate_1_raw] =
+      case
+      when jsondata[:streams][1].respond_to?(:key?) && jsondata[:streams][1].key?('bit_rate')
+        jsondata[:streams][1]['bit_rate']
+      when jsondata[:streams][1].respond_to?(:key?) && jsondata[:streams][1].key?(:tags) && jsondata[:streams][1][:tags].key?(:BPS)
+        jsondata[:streams][1][:tags][:BPS]
+      end # rubocop:disable Lint/EndAlignment
 
-    outhash[:bitrate_1_raw] = case
-    when jsondata[:streams][1].respond_to?(:key?) && jsondata[:streams][1].key?('bit_rate')
-      jsondata[:streams][1]['bit_rate']
-    when jsondata[:streams][1].respond_to?(:key?) && jsondata[:streams][1].key?(:tags) && jsondata[:streams][1][:tags].key?(:BPS)
-      jsondata[:streams][1][:tags][:BPS]
-    end # rubocop:disable Lint/EndAlignment
-
-    outhash[:bitrate_1] = case
-    when jsondata[:streams].length >= 2 && outhash[:bitrate_1_raw].to_i >= 1_000_000
-      Filesize.from(outhash[:bitrate_1_raw].to_s + 'b').to('Mb').round(2).to_s + 'Mb/s'
-    when jsondata[:streams].length >= 2 && outhash[:bitrate_1_raw].to_i.between?(1000, 999_999)
-      Filesize.from(outhash[:bitrate_1_raw].to_s + 'b').to('Kb').round.to_s + 'Kb/s'
-    when jsondata[:streams].length >= 2 && outhash[:bitrate_1_raw].to_i < 1000
-      outhash[:bitrate_1_raw].to_s + 'b/s'
-    end # rubocop:disable Lint/EndAlignment
+    outhash[:bitrate_1] = fs.call(outhash[:bitrate_1_raw])
+    # outhash[:bitrate_1] = case
+    # when jsondata[:streams].length >= 2 && outhash[:bitrate_1_raw].to_i >= 1_000_000
+    #   Filesize.from(outhash[:bitrate_1_raw].to_s + 'b').to('Mb').round(2).to_s + 'Mb/s'
+    # when jsondata[:streams].length >= 2 && outhash[:bitrate_1_raw].to_i.between?(1000, 999_999)
+    #   Filesize.from(outhash[:bitrate_1_raw].to_s + 'b').to('Kb').round.to_s + 'Kb/s'
+    # when jsondata[:streams].length >= 2 && outhash[:bitrate_1_raw].to_i < 1000
+    #   outhash[:bitrate_1_raw].to_s + 'b/s'
+    # end # rubocop:disable Lint/EndAlignment
 
     outhash[:type_1] = jsondata[:streams][1][:codec_type] if jsondata[:streams].length >= 2 && jsondata[:streams][1].respond_to?(:key?) && jsondata[:streams][1].key?(:codec_type)
 
     outhash[:codec_1] = jsondata[:streams][1][:codec_name] if jsondata[:streams].length >= 2 && jsondata[:streams][1].respond_to?(:key?) && jsondata[:streams][1].key?(:codec_name)
 
-    outhash[:height] = case
-    when jsondata[:streams][0][:height]
-      jsondata[:streams][0][:height]
-    when jsondata[:streams].length >= 2 && jsondata[:streams][1].respond_to?(:key) && jsondata[:streams][1].key?(:height)
-      jsondata[:streams][1][:height]
-    end # rubocop:disable Lint/EndAlignment
+    outhash[:height] =
+      case
+      when jsondata[:streams][0][:height]
+        jsondata[:streams][0][:height]
+      when jsondata[:streams].length >= 2 && jsondata[:streams][1].respond_to?(:key) && jsondata[:streams][1].key?(:height)
+        jsondata[:streams][1][:height]
+      end
 
-    outhash[:width] = case
-    when jsondata[:streams][0].key?(:width)
-      jsondata[:streams][0][:width]
-    when jsondata[:streams].length >= 2 && jsondata[:streams][1].respond_to?(:key) && jsondata[:streams][1].key?(:width)
-      jsondata[:streams][1][:width]
-    end # rubocop:disable Lint/EndAlignment
+    outhash[:width] =
+      case
+      when jsondata[:streams][0].key?(:width)
+        jsondata[:streams][0][:width]
+      when jsondata[:streams].respond_to?(:length) && jsondata[:streams].length >= 2 && jsondata[:streams][1].is_a?(Hash) && jsondata[:streams][1].key?(:width)
+        jsondata[:streams][1][:width]
+      end
 
-    begin
-      fr = ->(n) { calc.evaluate(n).to_f.round(2) }
-      outhash[:frame_rate] = case # Used 4 'when' statements for readability
+    outhash[:frame_rate] =
+      case # Used 4 'when' statements for readability
       when jsondata.respond_to?(:dig) && jsondata.dig(:streams, 0, :avg_frame_rate)
         fr.call(jsondata[:streams][0][:avg_frame_rate])
-      when !jsondata.respond_to?(:dig) && jsondata.key?(:streams) && jsondata[:streams].key?(0) && jsondata[:streams][0].key?(:avg_frame_rate)
+      when !jsondata.respond_to?(:dig) && jsondata.key?(:streams) && jsondata[:streams][0] && jsondata[:streams][0].key?(:avg_frame_rate)
         fr.call(jsondata[:streams][0][:avg_frame_rate])
       when jsondata.respond_to?(:dig) && jsondata.dig(:streams, 1, :avg_frame_rate)
         fr.call(jsondata[:streams][1][:avg_frame_rate])
       when !jsondata.respond_to?(:dig) && !jsondata[:streams][0].key?(:avg_frame_rate) && jsondata[:streams].length >= 2 && jsondata[:streams][1].key?(:avg_frame_rate)
         fr.call(jsondata[:streams][1][:avg_frame_rate])
-      end # rubocop:disable Ling/EndAlignment
-    rescue ZeroDivisionError
-      outhash[:frame_rate] = nil
-    end
+      end
     yield outhash if block_given?
     outhash
   end
@@ -222,9 +222,10 @@ module GenerateVideoInfo
     end
 
     def json(filepath, repo = nil)
+      raise 'First argument must be either a string or a pathname' unless filepath.is_a?(String) || filepath.is_a?(Pathname)
       out = nil
       Videojson.storage_names[repo] = 'videojson' if repo
-      filepath = Pathname.new(filepath) unless filepath.respond_to?(:exists)
+      filepath = Pathname.new(filepath) unless filepath.is_a?(Pathname)
       insert = Videojson.new
       # puts Videojson.count(filename: filepath.basename.to_s)
       if @db.storage_exists?('videojson') && Videojson.count(filename: filepath.basename.to_s) >= 1
