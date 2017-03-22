a61c5f26cea67df6b4dccf988154581b939d43b9
Andrew Nelson <andrew@andrewnelson.org>
2017-03-22 02:01:43 -0700

    Both cached and uncached json extraction now returns a pre-parsed Hash, remove some commented out code

--- andyrb/videoinfo/database/data.rb
+++ andyrb/videoinfo/database/data.rb
@@ -1,9 +1,11 @@
 # frozen_string_literal: true
 require 'data_mapper'
 require 'pathname'
+require 'json'
 
 require_relative '../probe'
 require_relative 'schema'
+require_relative '../../util/recursive_symbolize_keys'
 
 module VideoInfo
   module Database
@@ -21,32 +23,29 @@ module VideoInfo
       end
 
       def existing
-        # out = nil
-        # out = Videoinfo.all(fields: [:filename, :filehash]) if Videoinfo.count >= 1
-        # print out.inspect
         out = Videoinfo.count >= 1 ? Videoinfo.all(fields: [:filename, :filehash]) : nil
         out
       end
 
       def json(filepath, repo = nil)
-        raise 'First argument must be either a string or a pathname' unless filepath.is_a?(String) || filepath.is_a?(Pathname)
+        raise 'First argument must be either a string or a pathname object.' unless filepath.is_a?(String) || filepath.is_a?(Pathname)
         out = nil
         Videojson.storage_names[repo] = 'videojson' if repo
         filepath = Pathname.new(filepath) unless filepath.is_a?(Pathname)
+        filepath.freeze
         insert = Videojson.new
         # puts Videojson.count(filename: filepath.basename.to_s)
         if @db.storage_exists?('videojson') && Videojson.count(filename: filepath.basename.to_s) >= 1
           puts Mood.happy("Reading metadata from cache for #{filepath}") if @verbose
           out = Videojson.all(filename: filepath.basename, fields: [:jsondata])
-          out = out[0][:jsondata]
+          out = Util.recursive_symbolize_keys(JSON.parse(out[0][:jsondata]))
         else
           out = VideoInfo.probe(filepath.realpath.to_s, verbose: @verbose)
           begin
             puts Mood.happy("Caching JSON for #{filepath.basename}") if @verbose
             insert.attributes = { filename: filepath.basename, jsondata: out.to_json }
-            # print @vi.attributes
+            # print "#{@vi.attributes}\n"
             insert.save
-            # print "\n"
           rescue DataMapper::SaveFailureError
             insert.errors.each { |e| puts e } if @verbose
             raise "Save failure error raised for #{filepath.basename}" if @verbose
