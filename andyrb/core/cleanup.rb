747780dbcbc1044c45bc86e7393fc7698dd6d626
Andrew Nelson <andrew@andrewnelson.org>
2017-04-07 02:59:15 -0700

    Move previous monkey-patching methods to their own modules and running the base class/module's include method (or invoking it via .send if it's private) to insert the methods into the base class/module

--- andyrb/core/cleanup.rb
+++ andyrb/core/cleanup.rb
@@ -1,16 +1,20 @@
 # frozen_string_literal: true
 
-class Array
-  def cleanup!(unique: true)
-    flatten!
-    compact!
-    uniq! if unique
-  end
+module AndyCore
+  module Array
+    module Cleanup
+      def cleanup!(unique: true)
+        flatten!
+        compact!
+        uniq! if unique
+      end
 
-  def cleanup(unique: true)
-    out = flatten
-    out = compact
-    out = uniq if unique
-    out
+      def cleanup(unique: true)
+        out = flatten
+        out = compact
+        out = uniq if unique
+        out
+      end
+    end
   end
 end
--- andyrb/core/in.rb
+++ andyrb/core/in.rb
@@ -1,9 +1,13 @@
 # frozen_string_literal: true
 
 require_relative 'cleanup'
-class Object
-  def in?(*arr)
-    arr.cleanup!
-    arr.include? self
+Array.include AndyCore::Array::Cleanup
+
+module AndyCore
+  module Object
+    def in?(*arr)
+      arr.cleanup!
+      arr.include? self
+    end
   end
 end
