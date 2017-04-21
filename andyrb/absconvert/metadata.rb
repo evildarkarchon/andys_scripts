bcae00da11dbd8b09c2c6cdfc11a5f4ebc617aca
Andrew Nelson <andrew@andrewnelson.org>
2017-04-21 01:03:01 -0700

    Put the Metadata class from absconvert into the new ABSConvert module and add in database set up code since it no longer has the setup from the original script.

--- andyrb/absconvert/metadata.rb
+++ andyrb/absconvert/metadata.rb
@@ -1,51 +1,58 @@
 # frozen_string_literal: true
 
+require 'data_mapper'
+
 require_relative '../videoinfo/database'
 
-class Metadata
-  attr_reader :bitrates, :frame_rate
-  def initialize(filename)
-    path = File.basename(filename).freeze
-    @query = VideoInfo::Database::Videoinfo.all(filename: path, fields: %i[bitrate_0_raw type_0 bitrate_1_raw type_1 frame_rate])
-    @frame_rate = nil
-    @bitrates = {}
-    @bitrates[:video] = nil
-    @bitrates[:audio] = nil
-  end
+module ABSConvert
+  class Metadata
+    attr_reader :bitrates, :frame_rate
+    def initialize(filename, db, verbose: false)
+      raise TypeError, 'db must be a Pathname' unless db.is_a?(Pathname)
+      @verbose = verbose
+      DataMapper.setup(:default, "sqlite:#{db.realpath}")
+      DataMapper::Logger.new($stdout, :debug) if @verbose
+      path = File.basename(filename).freeze
+      @query = VideoInfo::Database::Videoinfo.all(filename: path, fields: %i[bitrate_0_raw type_0 bitrate_1_raw type_1 frame_rate])
+      @frame_rate = nil
+      @bitrates = {}
+      @bitrates[:video] = nil
+      @bitrates[:audio] = nil
+    end
 
-  def bitrate!
-    @bitrates[:video] =
-      case
-      when Args.videobitrate
-        Args.videobitrate
-      when @query[0][:type_0] == 'video'
-        @query[0][:bitrate_0_raw]
-      when @query[0][:type_1] == 'video'
-        @query[0][:bitrate_1_raw]
-      end
+    def bitrate!
+      @bitrates[:video] =
+        case
+        when Args.videobitrate
+          Args.videobitrate
+        when @query[0][:type_0] == 'video'
+          @query[0][:bitrate_0_raw]
+        when @query[0][:type_1] == 'video'
+          @query[0][:bitrate_1_raw]
+        end
 
-    @bitrates[:audio] =
-      case
-      when Args.audiobitrate
-        Args.audiobitrate
-      when @query[0][:type_0] == 'audio'
-        @query[0][:bitrate_0_raw]
-      when @query[0][:type_1] == 'audio'
-        @query[0][:bitrate_1_raw]
-      end
+      @bitrates[:audio] =
+        case
+        when Args.audiobitrate
+          Args.audiobitrate
+        when @query[0][:type_0] == 'audio'
+          @query[0][:bitrate_0_raw]
+        when @query[0][:type_1] == 'audio'
+          @query[0][:bitrate_1_raw]
+        end
+      @bitrates.freeze
+    end
 
-    @bitrates.freeze
-  end
+    def framerate!
+      @frame_rate =
+        case
+        when Args.framerate
+          Args.framerate
+        when @query[0][:frame_rate]
+          @query[0][:frame_rate]
+        end
 
-  def framerate!
-    @frame_rate =
-      case
-      when Args.framerate
-        Args.framerate
-      when @query[0][:frame_rate]
-        @query[0][:frame_rate]
-      end
-
-    @frame_rate.freeze
+      @frame_rate.freeze
+    end
   end
 end
