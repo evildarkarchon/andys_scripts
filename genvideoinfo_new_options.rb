9ff600f611a006fe057aab1284c276473ca24d4e
Andrew Nelson <andrew@andrewnelson.org>
2017-04-26 04:21:52 -0700

    Remove some commented out code and fix a bug.

--- genvideoinfo_new_options.rb
+++ genvideoinfo_new_options.rb
@@ -19,31 +19,7 @@ require_relative 'andyrb/options'
 Array.include AndyCore::Array::Cleanup unless Array.private_method_defined? :include
 Array.send(:include, AndyCore::Array::Cleanup) if Array.private_method_defined? :include
 
-# class Options
-#   def self.parse(args)
-#     options = OpenStruct.new
-#     options.db = Pathname.getwd + 'videoinfo.sqlite'
-#     options.debug = false
-#     options.verbose = false
-
-#     optparse = OptionParser.new do |opts|
-#       opts.on('--database file', 'Location where the database will be written') { |d| options.db = Pathname.new(d) }
-#       opts.on('-d', '--debug', "Don't actually do anything, just print what would be done.") do
-#         options.debug = true
-#         options.verbose = true
-#       end
-#       opts.on('-m', '--maintainence', 'Performs simple maintainence operations on the database.') { options.m = true }
-#       opts.on('-r', '--regen', 'Drops and recreates the videoinfo table (used for schema changes, testing, etc.)') { options.regen = true }
-#       opts.on('--reset-json', 'Drops and recreates the videojson table.') { options.reset_json = true }
-#       opts.on('--reset-all', 'Drops and recreates both the videoinfo and videojson tables.') { options.reset_all = true }
-#       opts.on('-v', '--verbose', 'Activates Verbose Mode') { options.verbose = true }
-#     end
-#     optparse.parse!(args)
-#     options
-#   end
-# end
-
-options = Options.new(ARGV.dup.cleanup!) do |defaults|
+opts = Options.new(ARGV.dup.cleanup) do |defaults|
   defaults[:db] = Pathname.getwd.join('videoinfo.sqlite')
   defaults[:debug] = false
   defaults[:verbose] = false
@@ -52,13 +28,13 @@ options = Options.new(ARGV.dup.cleanup!) do |defaults|
   defaults[:reset_json] = false
   defaults[:reset_all] = false
 end
-def options.files
-  out = super.source.dup
+def opts.files
+  out = @source.dup
   out = out.nil? || out.empty? ? [] : out
   out.keep_if { |f| File.file?(f) } unless nil? || empty?
   out.freeze
 end
-options.construct! do |h, i|
+opts.construct! do |h, i|
   h.on('--database [file]', String, 'Location where the database will be written') { |d| i[:db] = d }
   h.on('-d', '--debug', "Don't actually do anything, just print what would be done.") do
     i[:debug] = true
@@ -70,48 +46,20 @@ options.construct! do |h, i|
   h.on('-v', '--verbose', 'Activates verbose mode') { i[:verbose] = true }
   h.on('-m', '--mantainence', 'Vacuum the database.') { i[:maintainence] = true }
 end
+files = opts.files
 
-# args = lambda do
-#   arg = ARGV.dup.cleanup!
-#   outargs = Options.parse(arg)
-#   outfiles = arg.nil? || arg.empty? ? [] : arg.dup
-#   outfiles.keep_if { |f| File.file?(f) } unless nil? || empty?
-#   [outargs.freeze, outfiles.freeze]
-# end
-
-# Args, Files = args.call
-
-# Args = Options.parse(ARGV)
-
-# files = lambda do
-#   out = ARGV.nil? || ARGV.empty? ? [] : ARGV.dup
-#   out.cleanup! unless nil? || empty? || !is_a?(Array)
-#   out.keep_if { |f| File.file?(f) } unless nil? || empty?
-#   out
-# end
-
-# Files = files.call.freeze
-
-# Files = ARGV unless ARGV.nil? || ARGV.empty?
-# Files = [] if ARGV.nil? || ARGV.empty?
-# Files.cleanup! unless Files.nil? || Files.empty? || !Files.is_a?(Array)
-# Files.keep_if { |filename| File.file?(filename) } unless Files.nil? || Files.empty?
-
-if Files.nil? || Files.empty?
+if files.nil? || files.empty?
   wdpath = Pathname.getwd.find.to_a
   puts wdpath if Args.debug
   wdpath.keep_if(&:file?)
-  Files = wdpath.map(&:to_s)
-  puts Files if Args.debug
+  files = wdpath.map(&:to_s)
+  puts files if Args.debug
 end
-Args.freeze
 
 case
 when Args.debug
-  # puts Args
-  # puts Files
-  print "#{Args}\n"
-  print "#{Files}\n"
+  puts Args.inspect
+  puts files.inspect
 end
 
 FileUtils.touch(Args.db.basename.to_s) unless Args.db.file?
@@ -162,8 +110,8 @@ end
 
 initlist = []
 
-if defined?(Files) && Files && Files.is_a?(Array) && !Files.empty?
-  initlist = Files.map do |i|
+if defined?(files) && files && files.is_a?(Array) && !files.empty?
+  initlist = files.map do |i|
     path = Pathname.new(i).freeze
     dir = nil
     if path.directory?
