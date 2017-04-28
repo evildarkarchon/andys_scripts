d5768408353d3a0f6f9624319bdf9866249bb2f8
Andrew Nelson <andrew@andrewnelson.org>
2017-04-27 21:26:38 -0700

    Fix docstring for is_python_version

--- andypy/util/is_python_version.py
+++ andypy/util/is_python_version.py
@@ -8,13 +8,13 @@ from ..mood2 import Mood
 def is_python_version(query):
     """Helper function to take a tuple of the minimum version of python you are searching for and returning True if its greater than or equal to that version or False if its not.
 
-    It will convert any tuple-convertable types into a tuple for the purposes of evaluation.
+    It will convert any list-convertable types into a list for the purposes of evaluation.
 
-    query is a tuple of the minimum version of python that will return True."""
+    query is a list of the minimum version of python that will return True."""
 
     if not isinstance(query, (tuple, list, collections.deque)):
         # print("{} Value must be a tuple or a list.".format(Mood.sad()))
-        print(Mood.sad('Value must be a tuple or a list'))
+        print(Mood.sad('Value must be convertable to a list.'))
         raise TypeError
 
     if isinstance(query, tuple):
