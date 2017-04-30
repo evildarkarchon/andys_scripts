4bb76a1d75598772c8043a825ac5b7602bde30ca
Andrew Nelson <andrew@andrewnelson.org>
2017-04-29 21:22:30 -0700

    Add new cleanlist function that is a take on a similar function i made for ruby.

---
+++ andypy/util/cleanlist.py
@@ -0,0 +1,39 @@
+import itertools
+
+from ..mood2 import Mood
+
+
+def cleanlist(iterable, flatten=True, dedup=True, clean=True, debug=False, quiet=False):
+    out = iterable
+    if clean and not debug:
+        try:
+            out = [x for x in iterable if x is not None]
+        except TypeError:
+            if quiet:
+                print(Mood.neutral('Clean failed due to a type error, skipping.'))
+    elif clean and debug:
+        out = [x for x in iterable if x is not None]
+
+    if dedup and not debug:
+        try:
+            out = list(dict.fromkeys(out))
+        except TypeError:
+            if quiet:
+                print(Mood.neutral('De-Dup failed due to a type error, skipping.'))
+    elif clean and debug:
+        out = list(dict.fromkeys(out))
+
+    if flatten and not debug:
+        try:
+            out = list(itertools.chain.from_iterable(out))
+        except TypeError:
+            if quiet:
+                print(Mood.neutral('Flatten failed due to a type error, skipping.'))
+    elif flatten and debug:
+        out = list(itertools.chain.from_iterable(out))
+
+    '''if not isinstance(out, list):
+        return list(out)
+    else:
+        return out'''
+    return out
