953c43a7cbb2a10a04f7666c8c93335a71daafc0
Andrew Nelson <andrew@andrewnelson.org>
2017-05-05 03:35:03 -0700

    Add some more imports to resetvideoinfo_2017.py

--- resetvideoinfo_2017.py
+++ resetvideoinfo_2017.py
@@ -7,3 +7,8 @@ from sqlalchemy.orm import sessionmaker
 from andypy.mood2 import Mood
 from andypy.util.cleanlist import cleanlist
 from andypy.util.sortentries import sortentries
+from andypy.videoinfo.database.info import Info
+from andypy.videoinfo.database.session import sqa_session
+from andypy.videoinfo.database.schema import (VideoInfo, VideoJSON)
+from andypy.videoinfo.database.find import FindVideoInfo
+from andypy.videoinfo.database.videodata import VideoData
