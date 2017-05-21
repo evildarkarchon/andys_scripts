8afcc75d7671361602b4c5979093f75d97a9e270
Andrew Nelson <andrew@andrewnelson.org>
2017-05-21 04:17:57 -0700

    Modernize ttrss_update.py

--- ttrss_update.py
+++ ttrss_update.py
@@ -3,22 +3,30 @@
 import argparse
 import atexit
 import pathlib
+import shlex
 
-from andy.git import Git
-from andy.util import Mood, Program, Util
+from andypy.git import Git
+# from andypy.util import Mood, Program, Util
+from andypy.mood2 import Mood
+from andypy.program import Program
+from andypy.util.datediff import datediff
+from andypy.util.is_privileged import is_privileged
 
 args = argparse.ArgumentParser()
 args.add_argument("--aggressive", "-a", action="store_true", help="Run garbage collection in aggressive mode")
 args.add_argument("--replace", "-r", action="store_true", help="Rename any existing directory if a repository is not found.")
+args.add_argument("--restart-ttrssd", action="store_true", dest="restart", help="Restart ttrssd")
 options = vars(args.parse_args())
 
 ttrss = Git("/data/web/feeds", use_sudo=True, sudo_user="nginx")
 
 atexit.register(ttrss.clean_lock)
+if options["restart"]:
+    atexit.register(Program.runprogram, shlex.split("systemctl restart ttrssd"), use_sudo=True)
 
 
 def ttrssgcfile():
-    if not Util.is_privileged():
+    if not is_privileged():
         Program.runprogram(["touch", "/var/cache/ttrssgc"], use_sudo=True, user="root")
     else:
         pathlib.Path("/var/cache/ttrssgc").touch()
@@ -29,7 +37,7 @@ if not pathlib.Path("/var/cache/ttrssgc").is_file():
 
 
 def ttrssgc(then):
-    diff = Util.datediff(then)
+    diff = datediff(then)
     if diff.days > 30:
         ttrss.gc(aggressive=options["aggressive"])
         ttrssgcfile()
@@ -43,14 +51,14 @@ elif pathlib.Path("/data/web/feeds").exists() and not pathlib.Path("/data/web/fe
         Program.runprogram(["mv", "/data/web/feeds", "/data/web/feeds.old"], use_sudo=True, user="nginx")
         ttrss.clone("https://tt-rss.org/gitlab/fox/tt-rss.git")
     else:
-        print("{} No git repository located in the target directory.".format(Mood.sad()))
+        print(Mood.sad("No git repository located in the target directory."))
         raise FileNotFoundError
 elif pathlib.Path("/data/web/feeds").is_file():
-    print("{} Target location is a file, renaming and cloning repository.".format(Mood.sad()))
+    print(Mood.sad("Target location is a file, renaming and cloning repository."))
     Program.runprogram(["mv", "/data/web/feeds", "/data/web/feeds.bad"], use_sudo=True, user="nginx")
     ttrss.clone("https://tt-rss.org/gitlab/fox/tt-rss.git")
 else:
-    print("{} Target location does not exist, cloning repository.".format(Mood.neutral()))
+    print(Mood.neutral("{Target location does not exist, cloning repository."))
     ttrss.clone("https://tt-rss.org/gitlab/fox/tt-rss.git")
 
 ttrssgc(pathlib.Path("/var/cache/ttrssgc").stat().st_mtime)
