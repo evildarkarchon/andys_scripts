2ad9e78c4e4469ddf6167224598e09e6d7a52da2
Andrew Nelson <andrew@andrewnelson.org>
2017-05-21 17:45:49 -0700

    Add functionality to is_privileged.py to check a directory for writability.
    Also, instead of setting a default privuser and erroring out if its not
    a string or int, have an elif statement that checks if the effective uid
    is 0.

--- andypy/util/is_privileged.py
+++ andypy/util/is_privileged.py
@@ -1,20 +1,28 @@
 import os
 import pwd  # pylint: disable = e0401
+import pathlib
 
-from ..mood2 import Mood
 
-
-def is_privileged(privuser="root"):
+def is_privileged(privuser=None, directory=None):
         """Helper function to check if the current effective user is the same as the "privileged" user specified.
 
         privuser can be either a UID integer or a username string."""
 
-        if isinstance(privuser, str):
+        if not directory and isinstance(privuser, str):
             user = pwd.getpwnam(privuser)
-            return bool(user.pw_uid == os.geteuid())  # pylint: disable=e1101
-        elif isinstance(privuser, int):
-            return bool(privuser == os.geteuid())  # pylint: disable=e1101
-        else:
-            # print("{} User must be specified as a string or an integer".format(Mood.sad()))
-            print(Mood.sad("User must be specified as a string or an integer."))
-            raise TypeError
+            return bool(user.pw_uid == os.geteuid())  # pylint: disable=no-member
+        elif not directory and isinstance(privuser, int):
+            return bool(privuser == os.geteuid())  # pylint: disable=no-member
+        elif not directory and not isinstance(privuser, (str, int)):
+            return bool(os.geteuid() == 0)  # pylint: disable=no-member
+
+        if directory:
+            try:
+                test = pathlib.Path(directory).join('temp')  # pylint: disable=no-member
+                open(str(test), 'w')
+            except OSError:
+                return False
+            else:
+                if test.exists():
+                    test.delete()
+                return True
