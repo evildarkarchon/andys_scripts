f74480454401a90b15296390e93344dfdbefe49e
Andrew Nelson <andrew@andrewnelson.org>
2017-05-22 19:50:39 -0700

    Add ability to select between minidom or lxml.etree

--- andypy/prettyxml.py
+++ andypy/prettyxml.py
@@ -1,9 +1,18 @@
 import xml.dom.minidom as minidom
+import lxml.etree as etree
 
-def prettify_xml(input_xml):
-    try:
-        out = minidom.parse(input_xml)
-    except OSError:
-        out = minidom.parseString(input_xml)
-    else:
-        return out.toprettyxml()
+def prettify_xml(input_xml, parser='minidom'):
+    if 'minidom':
+        try:
+            out = minidom.parse(input_xml)
+        except OSError:
+            out = minidom.parseString(input_xml)
+        else:
+            return out.toprettyxml()
+    elif 'lxml':
+        try:
+            out = etree.parse(input_xml)
+        except OSError:
+            out = etree.fromstring(input_xml)
+        else:
+            return etree.tostring(out, pretty_print=True)
