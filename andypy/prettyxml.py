8deab8c03e8dbde80bdcadfb4ffa5073d39b2e35
Andrew Nelson <andrew@andrewnelson.org>
2017-05-24 03:35:50 -0700

    Make lxml an optional dependency, fix the conditionals and shut pylint up.

--- andypy/prettyxml.py
+++ andypy/prettyxml.py
@@ -1,18 +1,23 @@
 import xml.dom.minidom as minidom
-import lxml.etree as etree
+try:
+    import lxml.etree as etree
+except ImportError:
+    lxml_present = False
+else:
+    lxml_present = True
 
 def prettify_xml(input_xml, parser='minidom'):
-    if 'minidom':
+    if parser is 'minidom':
         try:
             out = minidom.parse(input_xml)
         except OSError:
             out = minidom.parseString(input_xml)
         else:
             return out.toprettyxml()
-    elif 'lxml':
+    elif lxml_present and parser is 'lxml':
         try:
-            out = etree.parse(input_xml)
+            out = etree.parse(input_xml)  # pylint:disable=no-member
         except OSError:
-            out = etree.fromstring(input_xml)
+            out = etree.fromstring(input_xml)  # pylint:disable=no-member
         else:
-            return etree.tostring(out, pretty_print=True)
+            return etree.tostring(out, pretty_print=True) # pylint:disable=no-member
