bbea21039f6425120f51e9464f474ae357260729
Andrew Nelson <andrew@andrewnelson.org>
2017-05-25 23:35:06 -0700

    Remove temp variable and do everything in the return statement.

--- andypy/util/sortfiles.py
+++ andypy/util/sortfiles.py
@@ -4,6 +4,4 @@ from .sortentries import sortentries
 
 
 def sortfiles(pathlist):
-    temp = [str(x) for x in pathlist if isinstance(x, pathlib.Path)]
-    temp = sortentries(temp)
-    return [pathlib.Path(x) for x in temp]
+    return [pathlib.Path(x) for x in sortentries([str(x) for x in pathlist if isinstance(x, pathlib.Path)])]
