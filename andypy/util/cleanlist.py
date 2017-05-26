7e15ad6766c73acfd9394af13b045aa15097ebb5
Andrew Nelson <andrew@andrewnelson.org>
2017-05-25 23:33:54 -0700

    Add docstring and quiet is now default for cleanlist()

--- andypy/util/cleanlist.py
+++ andypy/util/cleanlist.py
@@ -3,7 +3,20 @@ import itertools
 from ..mood2 import Mood
 
 
-def cleanlist(iterable, flatten=True, dedup=True, clean=True, debug=False, quiet=False):
+def cleanlist(iterable, flatten=True, dedup=True, clean=True, debug=False, quiet=True):
+    """
+    iterable is the iterable to be cleaned (usually a list)
+
+    flatten takes any nested lists and makes them into a single list (type errors here usually mean there are no nested lists)
+
+    dedup attempts to remove any duplicate entries from the iterable
+
+    clean removes any None entries from the iterable
+
+    debug disables all exception catching
+
+    quiet messages will not be printed when exceptions are caught (does nothing to uncaught exceptions)
+    """
     out = iterable
     if clean and not debug:
         try:
