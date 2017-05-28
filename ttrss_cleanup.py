82d11f9dc4fe5689590122bcdecb14e6cbfd2f72
Andrew Nelson <andrew@andrewnelson.org>
2017-05-28 02:09:30 -0700

    Modernize ttrss_cleanup.py

--- ttrss_cleanup.py
+++ ttrss_cleanup.py
@@ -3,16 +3,16 @@
 import pathlib
 from datetime import datetime
 
-from andy.util import Mood, Util
+from andypy.mood2 import Mood
+from andypy.util.datediff import datediff
+from andypy.util.sortfiles import sortfiles
 
-now = datetime.now()
-
-files = Util.sortentries(pathlib.Path("/data/ttrssbackup").rglob("feeds*.tar.xz"))
+files = sortfiles(pathlib.Path("/data/ttrssbackup").rglob("feeds*.tar.xz"))
 # print(sortentries(files))
 
 for filepath in files:
     then = datetime.fromtimestamp(filepath.stat().st_mtime)
-    filetime = now - then
+    filetime = datediff(then)
     if filetime.days > 14:
-        print("{} Removing {} from backup directory.".format(Mood.neutral(), filepath.name))
+        print(Mood.neutral("Removing {} from backup directory.".format(filepath.name)))
         filepath.unlink()
