3a0f98b4f064be0f3a8f90f5b7221c1bfaf55e88
Andrew Nelson <andrew@andrewnelson.org>
2017-05-30 04:31:09 -0700

    Add code to work around pwd not being present.
    It shouldn't be a problem on unix systems where this module will be
    used, but you never know. is_privileged() will return None if pwd is not
    present and we're not checking permissions on a directory. This is so
    that I can throw an exception if any check returns None.

--- andypy/util/is_privileged.py
+++ andypy/util/is_privileged.py
@@ -1,5 +1,10 @@
 import os
-import pwd  # pylint: disable = e0401
+try:
+    import pwd  # pylint: disable = e0401
+except ImportError:
+    pwdimport = False
+else:
+    pwdimport = True
 import pathlib
 
 
@@ -9,20 +14,24 @@ def is_privileged(privuser=None, directory=None):
         privuser can be either a UID integer or a username string."""
 
         if not directory and isinstance(privuser, str):
-            user = pwd.getpwnam(privuser)
-            return bool(user.pw_uid == os.geteuid())  # pylint: disable=no-member
+            if pwdimport:
+                user = pwd.getpwnam(privuser)
+                return bool(user.pw_uid == os.geteuid())  # pylint: disable=no-member
+            else:
+                return None
         elif not directory and isinstance(privuser, int):
             return bool(privuser == os.geteuid())  # pylint: disable=no-member
         elif not directory and not isinstance(privuser, (str, int)):
             return bool(os.geteuid() == 0)  # pylint: disable=no-member
 
+
         if directory:
             try:
-                test = pathlib.Path(directory).join('temp')  # pylint: disable=no-member
-                open(str(test), 'w')
+                test = pathlib.Path(directory).joinpath('temp').write_text('this is a test')  # pylint: disable=no-member
             except (OSError, PermissionError):
                 return False
             else:
+                return True
+            finally:
                 if test.exists():
                     test.delete()
-                return True
