b442cd4e6ba24632ab1d1de5a61706653d0cff26
Andrew Nelson <andrew@andrewnelson.org>
2017-06-17 00:00:52 -0700

    Flip placement of cleanup and cleanup!

--- andyrb/core/cleanup.rb
+++ andyrb/core/cleanup.rb
@@ -3,17 +3,17 @@
 module AndyCore
   module Array
     module Cleanup
-      def cleanup!(unique: true)
-        flatten!
-        compact!
-        uniq! if unique
-      end
-
       def cleanup(unique: true)
         out = flatten
         out = compact
         out = uniq if unique
         out
+      end
+
+      def cleanup!(unique: true)
+        flatten!
+        compact!
+        uniq! if unique
       end
     end
   end
