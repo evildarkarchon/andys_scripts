8193b1e6d2c744dcdac2d8796ed51d356628a8b8
Andrew Nelson <andrew@andrewnelson.org>
2017-06-19 01:52:17 -0700

    Remove construct!, rename opthash to default

--- andyrb/options.rb
+++ andyrb/options.rb
@@ -4,11 +4,11 @@ require 'optparse'
 
 class Options
   attr_reader :source, :args
-  def initialize(sourceargs, opthash = nil)
-    raise ValueError, 'You must supply either a code block or a hash.' unless block_given? || opthash
+  def initialize(sourceargs, default = nil)
+    raise ValueError, 'You must supply either a code block or a hash.' unless block_given? || default
     @args = {}
-    yield @args if block_given? && !opthash
-    @args = opthash if opthash && !block_given?
+    yield @args if block_given? && !default
+    @args = default if default && !block_given?
     raise TypeError, '@args must be either a hash or nil' unless @args.is_a?(Hash) || @args.nil?
     @source = sourceargs.is_a?(String) ? sourceargs.to_a : sourceargs
   end
@@ -18,10 +18,6 @@ class Options
     raise 'A block must be passed to this method.' unless block_given?
     yield optparse, @args if block_given?
     optparse.parse!(@source)
-  end
-
-  def construct!
-    raise 'The construct! method is obsolete, use parse_args!'
   end
 
   def [](key)
