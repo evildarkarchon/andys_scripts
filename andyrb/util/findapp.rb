0d437b80a1a80c9c6113505f9dbc415c88d00203
Andrew Nelson <andrew@andrewnelson.org>
2017-06-28 04:20:01 -0700

    Add functionality to raise an exception if the application is declared to be mandatory, defaults to false.

--- andyrb/util/findapp.rb
+++ andyrb/util/findapp.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 module Util
-  def self.findapp(cmd)
+  def self.findapp(cmd, mand = false)
     exe = nil
     exts = ENV['PATHEXT'] ? ENV['PATHEXT'].split(';') : ['']
     ENV['PATH'].split(File::PATH_SEPARATOR).each do |path|
@@ -10,13 +10,14 @@ module Util
         exe = which if File.executable?(which) && !File.directory?(which)
       end
     end
+    raise "Can't find application" if [exe.nil? || exe.empty?, mand].all?
     yield exe if block_given?
     exe
   end
   class FindApp
-    def self.which(cmd)
+    def self.which(cmd, mand = false)
       puts 'Util::FindApp.which is obsolete, use Util.findapp instead. Util.findapp will be called for compatibility.'
-      block_given? ? Util.findapp(cmd, &Proc.new) : Util.findapp(cmd)
+      block_given? ? Util.findapp(cmd, mand, &Proc.new) : Util.findapp(cmd)
     end
   end
 end
