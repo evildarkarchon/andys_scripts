17ef4f4bbc81dcb515ad0dbca0856a95e3d53ff3
Andrew Nelson <andrew@andrewnelson.org>
2017-06-28 04:09:54 -0700

    Move which method of FindApp to base Util module.
    Leaving FindApp.which behind with a warning that its deprecated. It will
    still run the code as it used to because it calls Util.findapp. When I
    feel that all users have moved away, I will remove the class, obviously
    any users that I missed will show up at that point.

--- andyrb/util/findapp.rb
+++ andyrb/util/findapp.rb
@@ -1,18 +1,22 @@
 # frozen_string_literal: true
 
 module Util
+  def self.findapp(cmd)
+    exe = nil
+    exts = ENV['PATHEXT'] ? ENV['PATHEXT'].split(';') : ['']
+    ENV['PATH'].split(File::PATH_SEPARATOR).each do |path|
+      exts.each do |ext|
+        which = File.join(path, "#{cmd}#{ext}")
+        exe = which if File.executable?(which) && !File.directory?(which)
+      end
+    end
+    yield exe if block_given?
+    exe
+  end
   class FindApp
     def self.which(cmd)
-      exe = nil
-      exts = ENV['PATHEXT'] ? ENV['PATHEXT'].split(';') : ['']
-      ENV['PATH'].split(File::PATH_SEPARATOR).each do |path|
-        exts.each do |ext|
-          which = File.join(path, "#{cmd}#{ext}")
-          exe = which if File.executable?(which) && !File.directory?(which)
-        end
-      end
-      yield exe if block_given?
-      exe
+      puts 'Util::FindApp.which is obsolete, use Util.findapp instead. Util.findapp will be called for compatibility.'
+      block_given? ? Util.findapp(cmd, &Proc.new) : Util.findapp(cmd)
     end
   end
 end
