8adef6f6a6838cad0d6ef60ba50ec62f8b34c840
Andrew Nelson <andrew@andrewnelson.org>
2017-06-30 22:36:49 -0700

    Add code to monkey-patch multiple methods/classes into a specified parent class

--- andyrb/core/monkeypatch.rb
+++ andyrb/core/monkeypatch.rb
@@ -1,7 +1,11 @@
 # frozen_string_literal: true
 
 module AndyCore
-  def self.monkeypatch(sourceclass, destclass)
-    sourceclass.private_method_defined?(:include) ? sourceclass.send(:include, destclass) : sourceclass.include(destclass)
+  def self.monkeypatch(parentclass, childclass)
+    if childclass.respond_to?(:each)
+      childclass.each { |i| parentclass.private_method_defined?(:include) ? parentclass.send(:include, i) : parentclass.include(i) }
+    else
+      parentclass.private_method_defined?(:include) ? parentclass.send(:include, childclass) : parentclass.include(childclass)
+    end
   end
 end
