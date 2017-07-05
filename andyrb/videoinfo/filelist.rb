4693381119ad4b1dc4d56c5de2577132585c06e4
Andrew Nelson <andrew@andrewnelson.org>
2017-07-05 15:43:07 -0700

    Port videoinfo's filelist to the new media_file method.

--- andyrb/videoinfo/filelist.rb
+++ andyrb/videoinfo/filelist.rb
@@ -4,18 +4,16 @@ require 'filemagic'
 
 require_relative '../core/sort'
 require_relative '../core/monkeypatch'
+require_relative '../util/media_file'
 
 AndyCore.monkeypatch(Array, AndyCore::Array::NatSort)
 
 module VideoInfo
   def self.genfilelist(filelist, testmode: false, sort: true)
     raise 'filelist must be an array or convertable to an array' unless filelist.respond_to?(:to_a)
-    whitelist = %w[video/x-flv video/mp4 video/mp2t video/3gpp video/quicktime video/x-msvideo video/x-ms-wmv video/webm video/x-matroska video/3gpp2 audio/x-wav]
-    whitelist += %w[audio/wave video/dvd video/mpeg application/vnd.rn-realmedia-vbr audio/vnd.rn-realaudio audio/x-realaudio]
-    magic = FileMagic.new(:mime_type)
     filelist.natsort! if sort
     puts 'Files to be examined:' if testmode
-    filelist.keep_if { |f| whitelist.include?(magic.file(f)) } unless testmode
+    filelist.keep_if { |f| Util.media_file?(f) } unless testmode
     case
     when testmode
       testlist = {}
@@ -26,7 +24,6 @@ module VideoInfo
       end
       puts testlist
     end
-    magic.close
     yield filelist if block_given?
     filelist
   end
